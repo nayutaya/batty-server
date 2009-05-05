0577220acbea95ba22d5a49c636733636686ac3e
okkez <okkez000@gmail.com>
2009-05-05 18:24:20 +0900

    ダッシュボードを仮実装

--- server/batty/app/controllers/home_controller.rb
+++ server/batty/app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 class HomeController < ApplicationController
   # GET /
   def index
-    # nop
+    @user = User.find_by_nickname('yu-yan')
+    @devices = @user.devices.sort_by(&:name)
   end
 end
--- server/batty/test/functional/home_controller_test.rb
+++ server/batty/test/functional/home_controller_test.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 require 'test_helper'
 
@@ -6,5 +7,13 @@ class HomeControllerTest < ActionController::TestCase
     base = {:controller => "home"}
 
     assert_routing("/", base.merge(:action => "index"))
+  end
+
+  # FIXME 認証実装後、他のユーザでもテストする
+  test "GET index by yu-yan" do
+    get :index
+    assert_equal('yu-yan', assigns(:user).nickname)
+    assert_equal('Touch Diamond', assigns(:devices)[0].name)
+    assert_equal('携帯電話', assigns(:devices)[1].name)
   end
 end
