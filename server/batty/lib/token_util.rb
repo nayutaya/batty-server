ee326beb1c354f2afc92c593659346ed4bc77564
Yuya Kato <yuyakato@gmail.com>
2009-05-08 00:53:30 +0900

    ライブラリをリファクタリング
    * コーディングスタイルを統一

--- server/batty/lib/token_util.rb
+++ server/batty/lib/token_util.rb
@@ -1,20 +1,18 @@
 
-
 module TokenUtil
-
   def self.create_token(size)
-    size.times.map{ rand(16).to_s(16) }.join
+    return size.times.map { rand(16).to_s(16) }.join
   end
 
   def self.create_unique_token(klass, column, size)
     begin
-      token = create_token(size)
+      token = self.create_token(size)
     end while klass.exists?(column => token)
-    token
+
+    return token
   end
 
   def self.create_token_regexp(size)
-    /\A[0-9a-f]{#{size}}\z/
+    return /\A[0-9a-f]{#{size}}\z/
   end
-
 end
