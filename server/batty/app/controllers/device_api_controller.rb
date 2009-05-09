4bd8eee6b3603d3c4a61389da438d5fed5ebad97
Yuya Kato <yuyakato@gmail.com>
2009-05-09 23:47:58 +0900

    エネルギー更新APIに、デバイストークンの処理を追加

--- server/batty/app/controllers/device_api_controller.rb
+++ server/batty/app/controllers/device_api_controller.rb
@@ -2,6 +2,7 @@
 # デバイスAPI
 class DeviceApiController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :required_param_device_token
 
   # FIXME: POSTメソッドに制限
   # POST /device/:device_token/energies/update/:level
@@ -10,6 +11,17 @@ class DeviceApiController < ApplicationController
     # TODO: Energyオブジェクトの生成処理を実装
     # TODO: Triggerオブジェクトの評価処理を実装
     # TODO: Eventオブジェクトの生成処理を実装
-    render(:text => "update_energy")
+    render(:text => "success")
+  end
+
+  private
+
+  def required_param_device_token(device_token = params[:device_token])
+    @device = Device.find_by_device_token(device_token)
+    return true if @device
+
+    render(:text => "", :status => 404)
+
+    return false
   end
 end
--- server/batty/test/functional/device_api_controller_test.rb
+++ server/batty/test/functional/device_api_controller_test.rb
@@ -10,4 +10,22 @@ class DeviceApiControllerTest < ActionController::TestCase
     assert_routing("/device/01234567/energies/update/01234/56789", base.merge(:action => "update_energy", :device_token => "01234567", :level => "01234", :time => "56789"))
     assert_routing("/device/89abcdef/energies/update/56789/01234", base.merge(:action => "update_energy", :device_token => "89abcdef", :level => "56789", :time => "01234"))
   end
+
+  test "POST update_energy" do
+    device = devices(:yuya_pda)
+
+    post :update_energy, :device_token => device.device_token
+
+    assert_response(:success)
+    assert_template(nil)
+
+    assert_equal(device, assigns(:device))
+  end
+
+  test "POST update_energy, abnormal, no device token" do
+    post :update_energy, :device_token => nil
+
+    assert_response(404)
+    assert_template(nil)
+  end
 end
