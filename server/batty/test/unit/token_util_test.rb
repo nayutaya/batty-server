ef30a7c08c1386404bce7edf590329056a8c3bf0
Yuya Kato <yuyakato@gmail.com>
2009-05-09 23:26:51 +0900

    TokenUtilのテストを追加
    * 呼び出す度に異なる値を返すテストを追加
    * 桁数のテストを追加
    * トークンの衝突のテストを整理

--- server/batty/test/unit/token_util_test.rb
+++ server/batty/test/unit/token_util_test.rb
@@ -8,23 +8,44 @@ class TokenUtilTest < ActiveSupport::TestCase
   end
 
   test "create_token" do
-    10.times {
-      assert_match(/\A[0-9a-f]{20}\z/, @mod.create_token(20))
-    }
+    assert_match(/\A[0-9a-f]{10}\z/, @mod.create_token(10))
+    assert_match(/\A[0-9a-f]{20}\z/, @mod.create_token(20))
+    assert_match(/\A[0-9a-f]{30}\z/, @mod.create_token(30))
+
+    # MEMO: 結果が乱数に依存するため、ごく稀にテストが失敗する可能性がある
+    assert_not_equal(
+      @mod.create_token(20),
+      @mod.create_token(20))
   end
 
   test "create_unique_token" do
+    assert_match(/\A[0-9a-f]{10}\z/, @mod.create_unique_token(User, :user_token, 10))
+    assert_match(/\A[0-9a-f]{20}\z/, @mod.create_unique_token(User, :user_token, 20))
+    assert_match(/\A[0-9a-f]{30}\z/, @mod.create_unique_token(User, :user_token, 30))
+
+    # MEMO: 結果が乱数に依存するため、ごく稀にテストが失敗する可能性がある
+    assert_not_equal(
+      @mod.create_unique_token(User, :user_token, 20),
+      @mod.create_unique_token(User, :user_token, 20))
+  end
+
+  test "create_unique_token, conflict" do
     [
-     [Device, "device_token", devices(:yuya_pda).device_token],
-     [User,   "user_token",   users(:yuya).user_token],
-    ].each { |klass, column, token|
-      tokens = [token, "b" * 20]
+     [Device, :device_token],
+     [User,   :user_token],
+    ].each { |klass, column|
+      exist_token  = klass.first[column]
+      unique_token = "0" * exist_token.size
+      assert_equal(true,  klass.exists?(column => exist_token))
+      assert_equal(false, klass.exists?(column => unique_token))
+
+      tokens = [exist_token, unique_token]
       musha = Kagemusha.new(@mod)
       musha.defs(:create_token) { tokens.shift }
       musha.swap {
         assert_equal(
-          "b" * 20,
-          @mod.create_unique_token(klass, column, 20))
+          unique_token,
+          @mod.create_unique_token(klass, column, exist_token.size))
       }
     }
   end
