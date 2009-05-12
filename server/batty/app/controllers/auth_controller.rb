f31a9d68ef6c4678e109716bb4ac622c73331a22
Yuya Kato <yuyakato@gmail.com>
2009-05-12 13:08:22 +0900

    ログイン完了画面を追加

--- server/batty/app/controllers/auth_controller.rb
+++ server/batty/app/controllers/auth_controller.rb
@@ -1,5 +1,9 @@
-class AuthController < ApplicationController
-  def login_complete
-  end
 
+# 認証
+class AuthController < ApplicationController
+  # GET ?
+  def login_complete
+    @return_path = params[:return_path]
+    @return_path = root_path if @return_path.blank?
+  end
 end
--- server/batty/test/functional/auth_controller_test.rb
+++ server/batty/test/functional/auth_controller_test.rb
@@ -1,8 +1,28 @@
+
 require 'test_helper'
 
 class AuthControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "routes" do
+    # TODO: 実装せよ
+  end
+
+  test "GET login_complete" do
+    return_path = "/return"
+
+    get :login_complete, :return_path => return_path
+
+    assert_response(:success)
+    assert_template("login_complete")
+
+    assert_equal(return_path, assigns(:return_path))
+  end
+
+  test "GET login_complete, without return path" do
+    get :login_complete
+
+    assert_response(:success)
+    assert_template("login_complete")
+
+    assert_equal(root_path, assigns(:return_path))
   end
 end
