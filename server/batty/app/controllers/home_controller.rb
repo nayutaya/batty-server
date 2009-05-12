95c4c12ab9ada90ba3a47c168a6af7c4e04401bd
Yuya Kato <yuyakato@gmail.com>
2009-05-12 15:13:08 +0900

    リファクタリング
    * 共通のコントローラの処理をフィルタ化
    * 共通のテストコードをメソッド化

--- server/batty/app/controllers/home_controller.rb
+++ server/batty/app/controllers/home_controller.rb
@@ -1,12 +1,19 @@
 
 # ホーム
 class HomeController < ApplicationController
+  before_filter :authenticate
+
   # GET /
   def index
-    @login_user = User.find_by_id(session[:user_id])
-
     if @login_user
       @devices = @login_user.devices.all(:order => "devices.name ASC")
     end
+  end
+
+  private
+
+  def authenticate(user_id = session[:user_id])
+    @login_user = User.find_by_id(user_id)
+    return true
   end
 end
--- server/batty/test/functional/home_controller_test.rb
+++ server/batty/test/functional/home_controller_test.rb
@@ -12,14 +12,13 @@ class HomeControllerTest < ActionController::TestCase
   end
 
   test "GET index, yuya" do
-    @request.session[:user_id] = users(:yuya).id
+    session_login(users(:yuya))
 
     get :index
 
     assert_response(:success)
     assert_template("index")
-
-    assert_equal(users(:yuya), assigns(:login_user))
+    assert_logged_in(users(:yuya))
 
     assert_equal(true, assigns(:devices).all? { |device| device.user == users(:yuya) })
     assert_equal(
@@ -28,26 +27,44 @@ class HomeControllerTest < ActionController::TestCase
   end
 
   test "GET index, shinya" do
-    @request.session[:user_id] = users(:shinya).id
+    session_login(users(:shinya))
 
     get :index
 
     assert_response(:success)
     assert_template("index")
-
-    assert_equal(users(:shinya), assigns(:login_user))
+    assert_logged_in(users(:shinya))
   end
 
   test "GET index, anonymous" do
-    @request.session[:user_id] = nil
+    session_logout
 
     get :index
 
     assert_response(:success)
-    assert_template("index")
-    
-    assert_equal(nil, assigns(:login_user))
+    assert_template("index")   
+    assert_not_logged_in
 
     assert_equal(nil, assigns(:devices))
+  end
+
+  private
+
+  def assert_logged_in(user)
+    assert_equal(user.id, @request.session[:user_id])
+    assert_equal(user, assigns(:login_user))
+  end
+
+  def assert_not_logged_in
+    assert_equal(nil, @request.session[:user_id])
+    assert_equal(nil, assigns(:login_user))
+  end
+
+  def session_login(user)
+    @request.session[:user_id] = user.id
+  end
+
+  def session_logout
+    @request.session[:user_id] = nil
   end
 end
