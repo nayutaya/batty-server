32a1fbd59771fdb8a6652825fe0a13d63450be84
Yuya Kato <yuyakato@gmail.com>
2009-05-12 15:46:31 +0900

    最新のエネルギー一覧、イベント一覧の出力処理を追加

--- server/batty/app/controllers/home_controller.rb
+++ server/batty/app/controllers/home_controller.rb
@@ -6,7 +6,9 @@ class HomeController < ApplicationController
   # GET /
   def index
     if @login_user
-      @devices = @login_user.devices.all(:order => "devices.name ASC")
+      @devices  = @login_user.devices.all(:order => "devices.name ASC, devices.id ASC")
+      @energies = @login_user.energies.paginate(:order => "energies.observed_at DESC, energies.id DESC", :page => 1, :per_page => 10)
+      @events   = @login_user.events.paginate(:order => "events.observed_at DESC, events.id DESC", :page => 1, :per_page => 10)
     end
   end
 
--- server/batty/test/functional/home_controller_test.rb
+++ server/batty/test/functional/home_controller_test.rb
@@ -3,6 +3,10 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  def setup
+    @yuya = users(:yuya)
+  end
+
   test "routes" do
     base = {:controller => "home"}
 
@@ -12,18 +16,35 @@ class HomeControllerTest < ActionController::TestCase
   end
 
   test "GET index, yuya" do
-    session_login(users(:yuya))
+    session_login(@yuya)
 
     get :index
 
     assert_response(:success)
     assert_template("index")
-    assert_logged_in(users(:yuya))
+    assert_logged_in(@yuya)
 
-    assert_equal(true, assigns(:devices).all? { |device| device.user == users(:yuya) })
+    assert_equal(@yuya.devices.size, assigns(:devices).size)
+    assert_equal(true, assigns(:devices).all? { |device| device.user == @yuya })
     assert_equal(
-      assigns(:devices).sort_by(&:name),
+      assigns(:devices).sort_by { |d| [d.name, d.id] },
       assigns(:devices))
+
+    assert_equal(@yuya.energies.size, assigns(:energies).total_entries)
+    assert_equal( 1, assigns(:energies).current_page)
+    assert_equal(10, assigns(:energies).per_page)
+    assert_equal(true, assigns(:energies).all? { |e| e.device.user == @yuya })
+    assert_equal(
+      assigns(:energies).sort_by { |e| [e.observed_at, e.id] }.reverse,
+      assigns(:energies))
+
+    assert_equal(@yuya.events.size, assigns(:events).total_entries)
+    assert_equal( 1, assigns(:events).current_page)
+    assert_equal(10, assigns(:events).per_page)
+    assert_equal(true, assigns(:events).all? { |e| e.device.user == @yuya })
+    assert_equal(
+      assigns(:events).sort_by { |e| [e.observed_at, e.id] }.reverse,
+      assigns(:events))
   end
 
   test "GET index, shinya" do
@@ -46,6 +67,8 @@ class HomeControllerTest < ActionController::TestCase
     assert_not_logged_in
 
     assert_equal(nil, assigns(:devices))
+    assert_equal(nil, assigns(:energies))
+    assert_equal(nil, assigns(:events))
   end
 
   private
