ad2a2bd66f655f079af0e5b3d9bc01bbd241ee67
Yuya Kato <yuyakato@gmail.com>
2009-05-12 11:50:50 +0900

    メールアドレスとパスワードによるログイン処理を追加

--- server/batty/app/controllers/password_auth_controller.rb
+++ server/batty/app/controllers/password_auth_controller.rb
@@ -1,9 +1,25 @@
 
 # メールアドレス/パスワード認証
 class PasswordAuthController < ApplicationController
+  # TODO: URL
+  # TODO: POSTメソッドに制限
   def login
+    # TODO: メールアドレスの確認処理を実装
+    # TODO: パスワードの確認処理を実装
+
+    credential = EmailCredential.authenticate(params[:email], params[:password])
+
+    if credential
+      session[:user_id] = credential.user_id
+      # TODO: リダイレクト処理を実装
+      redirect_to(:action => "login_complete")
+    else
+      render
+    end
   end
 
-  def logout
+  # TODO: URL
+  def login_complete
+    # TODO: リダイレクト処理を実装
   end
 end
--- server/batty/test/functional/password_auth_controller_test.rb
+++ server/batty/test/functional/password_auth_controller_test.rb
@@ -1,8 +1,41 @@
+
 require 'test_helper'
 
 class PasswordAuthControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "routes" do
+    # TODO: 実装せよ
+  end
+
+  test "POST login, yuya_gmail" do
+    assert_equal(nil, @request.session[:user_id])
+
+    post :login, :email => email_credentials(:yuya_gmail).email, :password => "yuya_gmail"
+
+    assert_response(:redirect)
+    assert_redirected_to(:controller => "password_auth", :action => "login_complete")
+
+    assert_equal(users(:yuya).id, @request.session[:user_id])
+  end
+
+  test "POST login, risa_example" do
+    assert_equal(nil, @request.session[:user_id])
+
+    post :login, :email => email_credentials(:risa_example).email, :password => "risa_example"
+
+    assert_response(:redirect)
+    assert_redirected_to(:controller => "password_auth", :action => "login_complete")
+
+    assert_equal(users(:risa).id, @request.session[:user_id])
+  end
+
+  test "POST login, authentication failed" do
+    assert_equal(nil, @request.session[:user_id])
+
+    post :login, :email => email_credentials(:yuya_gmail).email, :password => "invalid"
+
+    assert_response(:success)
+    assert_template("login")
+
+    assert_equal(nil, @request.session[:user_id])
   end
 end
