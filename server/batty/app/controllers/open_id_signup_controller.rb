f4160dac2f327933900661c5e4ae7a40f4774982
Yuya Kato <yuyakato@gmail.com>
2009-05-13 00:52:09 +0900

    OpenIDによるサインアップ処理をざっくりと実装
    * 最小限の正常系のみ
    * 未テスト

--- server/batty/app/controllers/open_id_signup_controller.rb
+++ server/batty/app/controllers/open_id_signup_controller.rb
@@ -1,9 +1,69 @@
 
 # OpenIDサインアップ
 class OpenIdSignupController < ApplicationController
+  # GET ?
   def index
+    session[:identity_url] = nil
+    @openid_url = nil
   end
 
+  # POST ?
+  # GET ?
+  def authenticate
+    @openid_url = params[:openid_url]
+
+    failed = proc { |message|
+      flash[:error] = message
+      redirect_to(:action => "index")
+    }
+
+    authenticate_with_open_id(@openid_url) { |result, identity_url, sreg|
+      case result.status
+      when :missing  then failed["OpenID サーバが見つかりませんでした。"]
+      when :invalid  then failed["OpenID が不正です。"]
+      when :canceled then failed["OpenID の検証がキャンセルされました。"]
+      when :failed   then failed["OpenID の検証が失敗しました。"]
+      when :successful
+        if OpenIdCredential.exists?(:identity_url => identity_url)
+          failed["指定されたOpenIDは既に登録されているため、利用できません。"]
+        else
+          session[:identity_url] = identity_url
+          redirect_to(:action => "authenticate_complete")
+        end
+      else raise("BUG")
+      end
+    }
+  end
+
+  # GET ?
+  def authenticate_complete
+    @identity_url = session[:identity_url]
+  end
+
+  # POST ?
   def signup
+    @identity_url = session[:identity_url]
+
+    User.transaction {
+      @user = User.new
+      @user.user_token = User.create_unique_user_token
+      @user.save!
+
+      @credential = OpenIdCredential.new
+      @credential.user         = @user
+      @credential.identity_url = @identity_url
+      @credential.loggedin_at  = Time.now
+      @credential.save!
+    }
+
+    session[:identity_url] = nil
+    session[:user_id]      = @user.id
+
+    redirect_to(:action => "signup_complete")
+  end
+
+  # GET ?
+  def signup_complete
+    # nop
   end
 end
--- server/batty/test/functional/open_id_signup_controller_test.rb
+++ server/batty/test/functional/open_id_signup_controller_test.rb
@@ -12,4 +12,21 @@ class OpenIdSignupControllerTest < ActionController::TestCase
     assert_response(:success)
     assert_template("index")
   end
+
+  test "POST authenticate, first step" do
+    post :authenticate, :openid_url => "livedoor.com"
+
+    assert_response(:redirect)
+  end
+
+  test "GET authenticate, second step" do
+    get :authenticate, :openid_url => nil
+  end
+
+  test "POST signup" do
+    post :signup
+
+    assert_response(:redirect)
+    assert_redirected_to(:controller => "open_id_signup", :action => "signup_complete")
+  end
 end
