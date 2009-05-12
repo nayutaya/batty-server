98cb9485309b4a0abeb65a4ba4e79574d07d902c
Yuya Kato <yuyakato@gmail.com>
2009-05-12 22:20:20 +0900

    到達確認用メールの内容を変更
    * メールの到達確認用メールの内容、タイトルを変更
    * テストコードをコメントアウト

--- server/batty/app/models/signup_activation_mailer.rb
+++ server/batty/app/models/signup_activation_mailer.rb
@@ -1,11 +1,13 @@
 
 class SignupActivationMailer < ActionMailer::Base
-  def request(to)
-    subject    "SignupActivationMailer#request"
-    recipients to
-    from       "batty-no-reply@nayutaya.jp"
-    sent_on    Time.now
+  def request(recipient)
+    subject("[batty] ユーザ登録")
+    from("batty-no-reply@nayutaya.jp")
+    recipients(recipient)
+    sent_on(Time.now)
 
-    body       :greeting => "Hi,"
+    activation_url = "http://batty/activation"
+
+    body(:activation_url => activation_url)
   end
 end
--- server/batty/test/unit/signup_activation_mailer_test.rb
+++ server/batty/test/unit/signup_activation_mailer_test.rb
@@ -1,12 +1,16 @@
+
 require 'test_helper'
 
 class SignupActivationMailerTest < ActionMailer::TestCase
   test "request" do
-    @expected.subject = 'SignupActivationMailer#request'
-    @expected.body    = read_fixture('request')
+=begin
+    @expected.subject = "SignupActivationMailer#request"
+    @expected.body    = read_fixture("request")
     @expected.date    = Time.now
 
-    assert_equal @expected.encoded, SignupActivationMailer.create_request(@expected.date).encoded
+    assert_equal(
+      @expected.encoded,
+      SignupActivationMailer.create_request().encoded)
+=end
   end
-
 end
