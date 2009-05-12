f42924407dd3e4788b6284b851f4ac2ebc2ac98b
Yuya Kato <yuyakato@gmail.com>
2009-05-13 02:06:33 +0900

    メールアドレス/パスワードによるサインアップを、一部試験的に実装

--- server/batty/app/controllers/password_signup_controller.rb
+++ server/batty/app/controllers/password_signup_controller.rb
@@ -1,6 +1,27 @@
 
 # メールアドレス/パスワードサインアップ
 class PasswordSignupController < ApplicationController
+  # GET ?
+  def index
+    @credential = EmailCredential.new
+  end
+
+  # POST ?
+  def confirm
+    @credential = EmailCredential.new
+    @credential.attributes = params[:credential]
+
+    @credential.activation_token = EmailCredential.create_unique_activation_token
+    @credential.hashed_password  = EmailCredential.create_hashed_password(@credential.password.to_s)
+
+    if @credential.valid?
+      session[:email_credential] = params[:credential]
+    else
+      flash.now[:error] = "内容を確認してください。"
+      render(:action => "index")
+    end
+  end
+
   def signup
   end
 end
--- server/batty/test/functional/password_signup_controller_test.rb
+++ server/batty/test/functional/password_signup_controller_test.rb
@@ -1,8 +1,22 @@
+
 require 'test_helper'
 
 class PasswordSignupControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "routes" do
+    # TODO:
+  end
+
+  test "GET index" do
+    get :index
+
+    assert_response(:success)
+    assert_template("index")
+  end
+
+  test "POST confirm" do
+    post :confirm, :credential => {:email => "a"}
+
+    assert_response(:success)
+    assert_template("confirm")
   end
 end
