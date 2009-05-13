36aeb2ed04d877cfea4c9d96869fe780b4d8fa81
Yuya Kato <yuyakato@gmail.com>
2009-05-13 11:18:52 +0900

    メールサインアップフォームに存在確認の検証を追加

--- server/batty/app/models/email_signup_form.rb
+++ server/batty/app/models/email_signup_form.rb
@@ -4,4 +4,8 @@ class EmailSignupForm < ActiveForm
   column :email,                 :type => :text
   column :password,              :type => :text
   column :password_confirmation, :type => :text
+
+  validates_presence_of :email
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
 end
--- server/batty/test/unit/email_signup_form_test.rb
+++ server/batty/test/unit/email_signup_form_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 class EmailSignupFormTest < ActiveSupport::TestCase
   def setup
     @klass = EmailSignupForm
+    @basic = @klass.new(
+      :email                 => "basic@example.com",
+      :password              => "password",
+      :password_confirmation => "password")
   end
 
   #
@@ -21,5 +25,28 @@ class EmailSignupFormTest < ActiveSupport::TestCase
       form.__send__("#{name}=", set_value)
       assert_equal(get_value, form.__send__(name))
     }
+  end
+
+  #
+  # 検証
+  #
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  test "validates_presence_of :email" do
+    @basic.email = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :password" do
+    @basic.password = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :password_confirmation" do
+    @basic.password_confirmation = nil
+    assert_equal(false, @basic.valid?)
   end
 end
