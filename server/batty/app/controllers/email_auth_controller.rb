3ec4f907873bf741627691cd2fd929764c4906fb
Yuya Kato <yuyakato@gmail.com>
2009-05-13 23:15:12 +0900

    メールアドレスによるログイン画面を追加

--- server/batty/app/controllers/email_auth_controller.rb
+++ server/batty/app/controllers/email_auth_controller.rb
@@ -1,3 +1,10 @@
 
 class EmailAuthController < ApplicationController
+  # GET auth/email
+  def index
+    @login_form = EmailLoginForm.new
+  end
+
+  # POST auth/email/login
+  # TODO: ログイン処理を実装
 end
--- server/batty/test/functional/email_auth_controller_test.rb
+++ server/batty/test/functional/email_auth_controller_test.rb
@@ -8,4 +8,16 @@ class EmailAuthControllerTest < ActionController::TestCase
     assert_routing("/auth/email",       base.merge(:action => "index"))
     assert_routing("/auth/email/login", base.merge(:action => "login"))
   end
+
+  test "GET index" do
+    get :index
+
+    assert_response(:success)
+    assert_template("index")
+    # FIXME: flashのテスト
+
+    assert_equal(
+      EmailLoginForm.new.attributes,
+      assigns(:login_form).attributes)
+  end
 end
