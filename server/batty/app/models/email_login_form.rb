80bc9c4a0666612184f747942a33b8aebb344acd
Yuya Kato <yuyakato@gmail.com>
2009-05-13 22:50:50 +0900

    メールログインフォームにカラムを追加

--- server/batty/app/models/email_login_form.rb
+++ server/batty/app/models/email_login_form.rb
@@ -1,2 +1,9 @@
-class EmailLoginForm < ActiveRecord::Base
+
+# メールログインフォーム
+class EmailLoginForm < ActiveForm
+  column :email,    :type => :text
+  column :password, :type => :text
+
+  # TODO: emailの存在を検証
+  # TODO: passwordの存在を検証
 end
--- server/batty/test/unit/email_login_form_test.rb
+++ server/batty/test/unit/email_login_form_test.rb
@@ -1,8 +1,32 @@
+
 require 'test_helper'
 
 class EmailLoginFormTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = EmailLoginForm
+  end
+
+  #
+  # 基底クラス
+  #
+
+  test "superclass" do
+    assert_equal(ActiveForm, @klass.superclass)
+  end
+
+  #
+  # カラム
+  #
+
+  test "columns" do
+    [
+      [:email,    nil, "str", "str"],
+      [:password, nil, "str", "str"],
+    ].each { |name, default, set_value, get_value|
+      form = @klass.new
+      assert_equal(default, form.__send__(name))
+      form.__send__("#{name}=", set_value)
+      assert_equal(get_value, form.__send__(name))
+    }
   end
 end
