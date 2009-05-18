6e2a5023f8d6d75d06030b24805e2c55111468f7
Yuya Kato <yuyakato@gmail.com>
2009-05-18 11:12:22 +0900

    デバイス詳細画面を追加

--- server/batty/app/controllers/devices_controller.rb
+++ server/batty/app/controllers/devices_controller.rb
@@ -1,9 +1,42 @@
 
 # デバイス
 class DevicesController < ApplicationController
+  before_filter :authentication, :only => [:show]
+  before_filter :authentication_required, :only => [:show]
+  before_filter :required_param_device_token, :only => [:show]
+
   # GET /devices/new
 
   # POST /devices/create
 
   # GET /device/:device_token
+  def show
+    # nop
+  end
+
+  private
+
+  def authentication(user_id = session[:user_id])
+    @login_user = User.find_by_id(user_id)
+    return true
+  end
+
+  def authentication_required
+    return true if @login_user
+
+    set_error("ログインが必要です。")
+    redirect_to(root_path)
+
+    return false
+  end
+
+  def required_param_device_token(device_token = params[:device_token])
+    @device = Device.find_by_device_token(device_token)
+    return true if @device
+
+    set_error("デバイストークンが正しくありません。")
+    redirect_to(root_path)
+
+    return false
+  end
 end
--- server/batty/test/functional/devices_controller_test.rb
+++ server/batty/test/functional/devices_controller_test.rb
@@ -2,6 +2,11 @@
 require 'test_helper'
 
 class DevicesControllerTest < ActionController::TestCase
+  def setup
+    @yuya     = users(:yuya)
+    @yuya_pda = devices(:yuya_pda)
+  end
+
   test "routes" do
     base = {:controller => "devices"}
 
@@ -10,5 +15,42 @@ class DevicesControllerTest < ActionController::TestCase
 
     assert_routing("/device/0123456789", base.merge(:action => "show", :device_token => "0123456789"))
     assert_routing("/device/abcdef",     base.merge(:action => "show", :device_token => "abcdef"))
+  end
+
+  # TODO: newアクションのテストを実装せよ
+
+  # TODO: createアクションのテストを実装せよ
+
+  test "GET show" do
+    session_login(@yuya)
+
+    get :show, :device_token => @yuya_pda.device_token
+
+    assert_response(:success)
+    assert_template("show")
+    assert_flash_empty
+    assert_logged_in(@yuya)
+
+    assert_equal(@yuya_pda, assigns(:device))
+  end
+
+  test "GET show, abnormal, no device token" do
+    session_login(@yuya)
+
+    get :show, :device_token => nil
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "GET show, abnormal, no login" do
+    session_logout
+
+    get :show, :device_token => @yuya_pda.device_token
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
   end
 end
