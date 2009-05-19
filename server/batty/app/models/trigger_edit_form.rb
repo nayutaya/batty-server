e4206b588c8c885ea6bb55a0f5b57af5b570b3de
Yuya Kato <yuyakato@gmail.com>
2009-05-19 22:11:02 +0900

    トリガ編集フォームに検証を追加

--- server/batty/app/models/trigger_edit_form.rb
+++ server/batty/app/models/trigger_edit_form.rb
@@ -4,4 +4,10 @@ class TriggerEditForm < ActiveForm
   column :enable,   :type => :boolean
   column :operator, :type => :integer
   column :level,    :type => :integer
+
+  validates_presence_of :enable
+  validates_presence_of :operator
+  validates_presence_of :level
+  validates_inclusion_of :operator, :in => 0..5, :allow_nil => true
+  validates_inclusion_of :level, :in => 0..100, :allow_nil => true
 end
--- server/batty/test/unit/trigger_edit_form_test.rb
+++ server/batty/test/unit/trigger_edit_form_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 class TriggerEditFormTest < ActiveSupport::TestCase
   def setup
     @klass = TriggerEditForm
+    @basic = @klass.new(
+      :enable   => true,
+      :operator => Trigger.operator_symbol_to_code(:eq),
+      :level    => 0)
   end
 
   #
@@ -29,6 +33,53 @@ class TriggerEditFormTest < ActiveSupport::TestCase
       assert_equal(default, form.__send__(name))
       form.__send__("#{name}=", set_value)
       assert_equal(get_value, form.__send__(name))
+    }
+  end
+
+  #
+  # 検証
+  #
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  test "validates_presence_of :enable" do
+    @basic.enable = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :operator" do
+    @basic.operator = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :level" do
+    @basic.level = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_inclusion_of :operator" do
+    [
+      [-1, false],
+      [ 0, true ],
+      [ 5, true ],
+      [ 6, false],
+    ].each { |value, expected|
+      @basic.operator = value
+      assert_equal(expected, @basic.valid?)
+    }
+  end
+
+  test "validates_inclusion_of :level" do
+    [
+      [ -1, false],
+      [  0, true ],
+      [100, true ],
+      [101, false],
+    ].each { |value, expected|
+      @basic.level = value
+      assert_equal(expected, @basic.valid?)
     }
   end
 end
