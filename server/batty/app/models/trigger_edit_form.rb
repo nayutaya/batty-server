35be532e556f17768dacf2704b415bafa337609c
Yuya Kato <yuyakato@gmail.com>
2009-05-19 22:00:27 +0900

    トリガ編集フォームにカラムを追加

--- server/batty/app/models/trigger_edit_form.rb
+++ server/batty/app/models/trigger_edit_form.rb
@@ -1,2 +1,7 @@
-class TriggerEditForm < ActiveRecord::Base
+
+# トリガ編集フォーム
+class TriggerEditForm < ActiveForm
+  column :enable,   :type => :boolean
+  column :operator, :type => :integer
+  column :level,    :type => :integer
 end
--- server/batty/test/unit/trigger_edit_form_test.rb
+++ server/batty/test/unit/trigger_edit_form_test.rb
@@ -1,8 +1,34 @@
+
 require 'test_helper'
 
 class TriggerEditFormTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = TriggerEditForm
+  end
+
+  #
+  # 基底クラス
+  #
+
+  test "superclass" do
+    assert_equal(ActiveForm, @klass.superclass)
+  end
+
+  #
+  # カラム
+  #
+
+  test "columns" do
+    [
+      [:enable,   nil, "1", true ],
+      [:enable,   nil, "0", false],
+      [:operator, nil, "1", 1],
+      [:level,    nil, "1", 1],
+    ].each { |name, default, set_value, get_value|
+      form = @klass.new
+      assert_equal(default, form.__send__(name))
+      form.__send__("#{name}=", set_value)
+      assert_equal(get_value, form.__send__(name))
+    }
   end
 end
