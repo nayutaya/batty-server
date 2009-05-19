e8f272cd042141a09da250a28e027a1ce03ca582
Yuya Kato <yuyakato@gmail.com>
2009-05-19 23:42:00 +0900

    トリガ追加画面にフォームを追加

--- server/batty/app/controllers/triggers_controller.rb
+++ server/batty/app/controllers/triggers_controller.rb
@@ -7,6 +7,14 @@ class TriggersController < ApplicationController
 
   # GET /device/:device_token/triggers/new
   def new
-    # nop
+    @edit_form = TriggerEditForm.new
+    set_operators_for_select
+  end
+
+  private
+
+  def set_operators_for_select
+    @operators_for_select = TriggerEditForm.operators_for_select(
+      :include_blank => true)
   end
 end
--- server/batty/test/functional/triggers_controller_test.rb
+++ server/batty/test/functional/triggers_controller_test.rb
@@ -6,6 +6,8 @@ class TriggersControllerTest < ActionController::TestCase
     @yuya = users(:yuya)
     @yuya_pda = devices(:yuya_pda)
 
+    @edit_form = TriggerEditForm.new
+
     session_login(@yuya)
   end
 
@@ -25,6 +27,14 @@ class TriggersControllerTest < ActionController::TestCase
     assert_logged_in(@yuya)
 
     assert_equal(@yuya_pda, assigns(:device))
+
+    assert_equal(
+      TriggerEditForm.new.attributes,
+      assigns(:edit_form).attributes)
+
+    assert_equal(
+      TriggerEditForm.operators_for_select(:include_blank => true),
+      assigns(:operators_for_select))
   end
 
   test "GET new, abnormal, no device token" do
