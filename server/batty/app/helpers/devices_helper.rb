a6fafda19b4ceb2d3d8156457a9302af819a43b0
Yuya Kato <yuyakato@gmail.com>
2009-05-19 21:10:52 +0900

    デバイス追加画面のデザインを変更

--- server/batty/app/helpers/devices_helper.rb
+++ server/batty/app/helpers/devices_helper.rb
@@ -1,10 +1,20 @@
 
 module DevicesHelper
   def device_icon24(device_icon)
-    return image_tag(device_icon.url24, :width => 24, :height => 24, :alt => device_icon.name)
+    return image_tag(
+      device_icon.url24,
+      :width  => 24,
+      :height => 24,
+      :alt    => device_icon.name,
+      :title  => device_icon.name)
   end
 
   def device_icon48(device_icon)
-    return image_tag(device_icon.url48, :width => 48, :height => 48, :alt => device_icon.name)
+    return image_tag(
+      device_icon.url48,
+      :width  => 48,
+      :height => 48,
+      :alt    => device_icon.name,
+      :title  => device_icon.name)
   end
 end
