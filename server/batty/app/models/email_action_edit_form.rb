760c189581c83c14be23e0d7b31c9989d391f7c2
Yuya Kato <yuyakato@gmail.com>
2009-05-20 12:15:06 +0900

    メールアクション編集フォームにカラムを追加

--- server/batty/app/models/email_action_edit_form.rb
+++ server/batty/app/models/email_action_edit_form.rb
@@ -1,2 +1,8 @@
-class EmailActionEditForm < ActiveRecord::Base
+
+# メールアクション編集フォーム
+class EmailActionEditForm < ActiveForm
+  column :enable,  :type => :boolean
+  column :email,   :type => :text
+  column :subject, :type => :text
+  column :body,    :type => :text
 end
--- server/batty/test/unit/email_action_edit_form_test.rb
+++ server/batty/test/unit/email_action_edit_form_test.rb
@@ -1,8 +1,35 @@
+
 require 'test_helper'
 
 class EmailActionEditFormTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = EmailActionEditForm
+  end
+
+  #
+  # 基底クラス
+  #
+
+  test "superclass" do
+    assert_equal(ActiveForm, @klass.superclass)
+  end
+
+  #
+  # カラム
+  #
+
+  test "columns" do
+    [
+      [:enable,  nil, "1", true ],
+      [:enable,  nil, "0", false],
+      [:email,   nil, "1", "1"],
+      [:subject, nil, "1", "1"],
+      [:body,    nil, "1", "1"],
+    ].each { |name, default, set_value, get_value|
+      form = @klass.new
+      assert_equal(default, form.__send__(name))
+      form.__send__("#{name}=", set_value)
+      assert_equal(get_value, form.__send__(name))
+    }
   end
 end
