73face86e8017c892e3cd120fc02362dc598bc96
Yuya Kato <yuyakato@gmail.com>
2009-05-21 01:43:57 +0900

    メールアクション編集フォームに検証を追加

--- server/batty/app/models/email_action_edit_form.rb
+++ server/batty/app/models/email_action_edit_form.rb
@@ -5,4 +5,12 @@ class EmailActionEditForm < ActiveForm
   column :email,   :type => :text
   column :subject, :type => :text
   column :body,    :type => :text
+
+  validates_presence_of :email
+  validates_presence_of :subject
+  validates_presence_of :body
+  validates_length_of :email, :maximum => 200, :allow_nil => true
+  validates_length_of :subject, :maximum => 200, :allow_nil => true
+  validates_length_of :body, :maximum => 1000, :allow_nil => true
+  validates_email_format_of :email
 end
--- server/batty/test/unit/email_action_edit_form_test.rb
+++ server/batty/test/unit/email_action_edit_form_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 class EmailActionEditFormTest < ActiveSupport::TestCase
   def setup
     @klass = EmailActionEditForm
+    @basic = @klass.new(
+      :email   => "email@example.jp",
+      :subject => "subject",
+      :body    => "Body")
   end
 
   #
@@ -30,6 +34,79 @@ class EmailActionEditFormTest < ActiveSupport::TestCase
       assert_equal(default, form.__send__(name))
       form.__send__("#{name}=", set_value)
       assert_equal(get_value, form.__send__(name))
+    }
+  end
+
+  #
+  # 検証
+  #
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  test "validates_presence_of :email" do
+    @basic.email = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :subject" do
+    @basic.subject = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :body" do
+    @basic.body = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_length_of :email" do
+    # MEMO: 下記の制約を満たしつつ、文字列長の検証のテストを行う
+    #       * ローカルパートは64文字以内
+    #       * ドメインパートは255文字以内
+    #       * ドメインパートのドットで区切られたパートは63文字以内
+
+    [
+      ["a@b.c.d.com", 11, true ],
+      [["a" * 64 + "@" + "b" * 63, "c" * 63, "d" * 3, "com"].join("."), 200, true ],
+      [["a" * 64 + "@" + "b" * 63, "c" * 63, "d" * 4, "com"].join("."), 201, false],
+    ].each { |value, length, expected|
+      assert_equal(length, value.size)
+      @basic.email = value
+      assert_equal(expected, @basic.valid?, value)
+    }
+  end
+
+  test "validates_length_of :subject" do
+    [
+      ["あ" *   1, true ],
+      ["あ" * 200, true ],
+      ["あ" * 201, false],
+    ].each { |value, expected|
+      @basic.subject = value
+      assert_equal(expected, @basic.valid?, value)
+    }
+  end
+
+  test "validates_length_of :body" do
+    [
+      ["あ" *    1, true ],
+      ["あ" * 1000, true ],
+      ["あ" * 1001, false],
+    ].each { |value, expected|
+      @basic.body = value
+      assert_equal(expected, @basic.valid?, value)
+    }
+  end
+
+  test "validates_email_format_of :email" do
+    [
+      ["foo@example.com",   true ],
+      ["foo@example.co.jp", true ],
+      ["foo@example",       false],
+    ].each { |value, expected|
+      @basic.email = value
+      assert_equal(expected, @basic.valid?, value)
     }
   end
 end
