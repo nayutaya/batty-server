1a71cc61956ca1edee1e5872c0b36a3fb1584edc
Yuya Kato <yuyakato@gmail.com>
2009-05-21 21:25:56 +0900

    イベント通知メールの作成処理を追加

--- server/batty/app/models/event_notification.rb
+++ server/batty/app/models/event_notification.rb
@@ -1,13 +1,31 @@
-class EventNotification < ActionMailer::Base
-  
 
-  def notify(sent_at = Time.now)
-    subject    'EventNotification#notify'
-    recipients ''
-    from       ''
-    sent_on    sent_at
-    
-    body       :greeting => 'Hi,'
+# イベント通知メール
+class EventNotification < ActionMailer::Base
+  def self.create_notify_params(options)
+    options = options.dup
+    subject    = options.delete(:subject)    || raise(ArgumentError)
+    recipients = options.delete(:recipients) || raise(ArgumentError)
+    body       = options.delete(:body)       || raise(ArgumentError)
+    raise(ArgumentError) unless options.empty?
+
+    return {
+      :header => {
+        :subject    => subject,
+        :from       => "batty-no-reply@nayutaya.jp",
+        :recipients => recipients,
+      },
+      :body   => {
+        :body => body,
+      },
+    }
   end
 
+  def notify(options)
+    params = self.class.create_notify_params(options)
+    subject(params[:header][:subject])
+    from(params[:header][:from])
+    recipients(params[:header][:recipients])
+    sent_on(Time.now)
+    body(params[:body])
+  end
 end
--- server/batty/test/unit/event_notification_test.rb
+++ server/batty/test/unit/event_notification_test.rb
@@ -1,12 +1,59 @@
+
 require 'test_helper'
 
 class EventNotificationTest < ActionMailer::TestCase
-  test "notify" do
-    @expected.subject = 'EventNotification#notify'
-    @expected.body    = read_fixture('notify')
-    @expected.date    = Time.now
-
-    assert_equal @expected.encoded, EventNotification.create_notify(@expected.date).encoded
+  def setup
+    @klass = EventNotification
   end
 
+  test "self.create_notify_params" do
+    options = {
+      :subject    => "subject",
+      :recipients => "recipients@example.com",
+      :body       => "body",
+    }
+    expected = {
+      :header => {
+        :subject    => "subject",
+        :from       => "batty-no-reply@nayutaya.jp",
+        :recipients => "recipients@example.com",
+      },
+      :body   => {
+        :body => "body",
+      },
+    }
+    assert_equal(expected, @klass.create_notify_params(options))
+  end
+
+  test "self.create_notify_params, deficient parameter" do
+    assert_nothing_raised {
+      @klass.create_notify_params(:subject => "", :recipients => "", :body => "")
+    }
+    assert_raise(ArgumentError) {
+      @klass.create_notify_params(:recipients => "", :body => "")
+    }
+    assert_raise(ArgumentError) {
+      @klass.create_notify_params(:subject => "", :body => "")
+    }
+    assert_raise(ArgumentError) {
+      @klass.create_notify_params(:subject => "", :recipients => "")
+    }
+  end
+
+  test "self.create_notify_params, invalid parameter" do
+    assert_raise(ArgumentError) {
+      @klass.create_notify_params(:invalid => true)
+    }
+  end
+
+  test "notify" do
+    options = {
+      :subject    => "subject",
+      :recipients => email_addresses(:yuya1).email,
+      :body       => "body",
+    }
+    assert_nothing_raised {
+      @klass.create_notify(options).encoded
+    }
+  end
 end
