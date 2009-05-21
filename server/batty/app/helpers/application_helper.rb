6577702ba11aec8c8fd6dcd1f6000a60370060de
Yuya Kato <yuyakato@gmail.com>
2009-05-21 14:55:52 +0900

    トップ画面にRSS Auto-discoveryを追加

--- server/batty/app/helpers/application_helper.rb
+++ server/batty/app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 
 module ApplicationHelper
+  def rss_auto_discovery(options = {})
+    options = options.dup
+    title = options.delete(:title) || raise(ArgumentError)
+    href  = options.delete(:href)  || raise(ArgumentError)
+    raise(ArgumentError) unless options.empty?
+
+    return %|<link rel="alternate" type="application/rss+xml" title="#{h(title)}" href="#{h(href)}" />|
+  end
+
   def enable_icon
     return image_tag("icons/fam/tick.png", :width => 16, :height => 16, :alt => "有効", :title => "有効")
   end
