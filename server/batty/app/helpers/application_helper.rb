c472ad79299a97a50b9a329074edf7ac2b1906c3
Yuya Kato <yuyakato@gmail.com>
2009-05-21 14:40:22 +0900

    デバイス画面に有効/無効のアイコンを追加

--- server/batty/app/helpers/application_helper.rb
+++ server/batty/app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
-# Methods added to this helper will be available to all templates in the application.
+
 module ApplicationHelper
+  def enable_icon
+    return image_tag("icons/fam/tick.png", :width => 16, :height => 16, :alt => "有効", :title => "有効")
+  end
+
+  def disable_icon
+    return image_tag("icons/fam/delete.png", :width => 16, :height => 16, :alt => "無効", :title => "無効")
+  end
+
+  def enable_or_disable_icon(enable)
+    return (enable ? enable_icon : disable_icon)
+  end
 end
