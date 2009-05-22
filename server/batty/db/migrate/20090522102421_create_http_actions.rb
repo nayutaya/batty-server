6c0631262c072e4a63a4a9ac03539bbd32901134
Yuya Kato <yuyakato@gmail.com>
2009-05-22 19:41:21 +0900

    HTTPアクションモデルのDB定義を追加

--- server/batty/db/migrate/20090522102421_create_http_actions.rb
+++ server/batty/db/migrate/20090522102421_create_http_actions.rb
@@ -1,9 +1,25 @@
+
 class CreateHttpActions < ActiveRecord::Migration
   def self.up
     create_table :http_actions do |t|
-
-      t.timestamps
+      # 作成日時
+      t.datetime :created_at,  :null => false
+      # 更新日時
+      t.datetime :updated_at,  :null => false
+      # トリガID
+      t.integer  :trigger_id,  :null => false
+      # 有効/無効
+      t.boolean  :enable,      :null => false
+      # HTTPメソッド
+      t.string   :http_method, :null => false, :limit => 10
+      # URL
+      t.string   :url,         :null => false, :limit => 200
+      # POSTデータ
+      t.text     :body,        :null => true
     end
+
+    add_index :http_actions, :trigger_id
+    add_index :http_actions, :enable
   end
 
   def self.down
