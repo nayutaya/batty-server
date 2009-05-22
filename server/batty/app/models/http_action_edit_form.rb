71f95abb12398ef218f537b94fdf10bd340dee93
Yuya Kato <yuyakato@gmail.com>
2009-05-23 00:15:36 +0900

    リファクタリング

--- server/batty/app/models/http_action_edit_form.rb
+++ server/batty/app/models/http_action_edit_form.rb
@@ -8,8 +8,8 @@ class HttpActionEditForm < ActiveForm
 
   validates_presence_of :http_method
   validates_presence_of :url
-  validates_length_of :url, :maximum => 200, :allow_nil => true
-  validates_length_of :body, :maximum => 1000, :allow_nil => true
-  validates_inclusion_of :http_method, :in => %w[HEAD GET POST], :allow_nil => true
+  validates_length_of :url, :maximum => HttpAction::UrlMaximumLength, :allow_nil => true
+  validates_length_of :body, :maximum => HttpAction::BodyMaximumLength, :allow_nil => true
+  validates_inclusion_of :http_method, :in => HttpAction::HttpMethods, :allow_nil => true
   validates_format_of :url, :with => URI.regexp(["http"]), :allow_nil => true
 end
