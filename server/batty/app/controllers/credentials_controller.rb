9fbce146d43f15f8148a08c0cc248462f053a97f
Yuya Kato <yuyakato@gmail.com>
2009-05-25 23:33:50 +0900

    ログイン情報の一覧処理を追加

--- server/batty/app/controllers/credentials_controller.rb
+++ server/batty/app/controllers/credentials_controller.rb
@@ -1,5 +1,14 @@
-class CredentialsController < ApplicationController
-  def index
-  end
 
+# ログイン情報コントローラ
+class CredentialsController < ApplicationController
+  before_filter :authentication
+  before_filter :authentication_required
+
+  # GET /credentials
+  def index
+    @open_id_credentials = @login_user.open_id_credentials.all(
+      :order => "open_id_credentials.identity_url ASC")
+    @email_credentials = @login_user.email_credentials.all(
+      :order => "email_credentials.email ASC")
+  end
 end
