6e04776f971cd659356ad29facad8177c247ef90
Yuya Kato <yuyakato@gmail.com>
2009-05-26 01:08:45 +0900

    追加を示すアイコンを追加

--- server/batty/app/helpers/application_helper.rb
+++ server/batty/app/helpers/application_helper.rb
@@ -10,15 +10,34 @@ module ApplicationHelper
   end
 
   def enable_icon
-    return image_tag("icons/fam/tick.png", :width => 16, :height => 16, :alt => "有効", :title => "有効")
+    return image_tag(
+      "icons/fam/tick.png",
+      :width  => 16,
+      :height => 16,
+      :alt    => "有効",
+      :title  => "有効")
   end
 
   def disable_icon
-    return image_tag("icons/fam/stop.png", :width => 16, :height => 16, :alt => "無効", :title => "無効")
+    return image_tag(
+      "icons/fam/stop.png",
+      :width  => 16,
+      :height => 16,
+      :alt    => "無効",
+      :title  => "無効")
   end
 
   def enable_or_disable_icon(enable)
     return (enable ? enable_icon : disable_icon)
+  end
+
+  def add_icon
+    return image_tag(
+      "icons/fam/add.png",
+      :width  => 16,
+      :height => 16,
+      :alt    => "追加",
+      :title  => "追加")
   end
 
   def edit_icon
