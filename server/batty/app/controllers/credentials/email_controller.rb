d22284a7a4228b4e285712f624422a0dbc2243cd
Yuya Kato <yuyakato@gmail.com>
2009-05-27 00:33:28 +0900

    パスワードの変更処理を追加

--- server/batty/app/controllers/credentials/email_controller.rb
+++ server/batty/app/controllers/credentials/email_controller.rb
@@ -1,10 +1,14 @@
 
 # メールログイン情報コントローラ
 class Credentials::EmailController < ApplicationController
+  verify(
+    :method => :post,
+    :render => {:text => "Method Not Allowed", :status => 405},
+    :only   => [:update_password])
   before_filter :authentication
   before_filter :authentication_required
-  before_filter :required_param_email_credential_id, :only => [:edit_password]
-  before_filter :specified_email_credential_belongs_to_login_user, :only => [:edit_password]
+  before_filter :required_param_email_credential_id, :only => [:edit_password, :update_password]
+  before_filter :specified_email_credential_belongs_to_login_user, :only => [:edit_password, :update_password]
 
   # GET /credential/email/:email_credential_id/edit_password
   def edit_password
@@ -12,7 +16,21 @@ class Credentials::EmailController < ApplicationController
   end
 
   # POST /credential/email/:email_credential_id/update_password
-  # TODO: 実装せよ
+  def update_password
+    @edit_form = EmailPasswordEditForm.new(params[:edit_form])
+
+    @email_credential.attributes = @edit_form.to_email_credential_hash
+
+    if @edit_form.valid? && @email_credential.save
+      set_notice("パスワードを変更しました。")
+      redirect_to(:controller => "/credentials")
+    else
+      @edit_form.password              = nil
+      @edit_form.password_confirmation = nil
+      set_error_now("入力内容を確認してください。")
+      render(:action => "edit_password")
+    end
+  end
 
   private
 
--- server/batty/test/functional/credentials/email_controller_test.rb
+++ server/batty/test/functional/credentials/email_controller_test.rb
@@ -7,6 +7,10 @@ class Credentials::EmailControllerTest < ActionController::TestCase
     @yuya_gmail   = email_credentials(:yuya_gmail)
     @risa_example = email_credentials(:risa_example)
 
+    @edit_form = EmailPasswordEditForm.new(
+      :password              => "password",
+      :password_confirmation => "password")
+
     session_login(@yuya)
   end
 
@@ -52,6 +56,75 @@ class Credentials::EmailControllerTest < ActionController::TestCase
 
   test "GET edit_password, abnormal, other's email credential" do
     get :edit_password, :email_credential_id => @risa_example.id
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "POST update_password" do
+    assert_equal(true, @edit_form.valid?)
+
+    post :update_password, :email_credential_id => @yuya_gmail.id, :edit_form => @edit_form.attributes
+
+    assert_response(:redirect)
+    assert_redirected_to(:controller => "/credentials", :action => "index")
+    assert_flash_notice
+    assert_logged_in(@yuya)
+
+    assert_equal(@yuya_gmail, assigns(:email_credential))
+
+    assert_equal(
+      @edit_form.attributes,
+      assigns(:edit_form).attributes)
+
+    assigns(:email_credential).reload
+    assert_equal(
+      true,
+      EmailCredential.compare_hashed_password(@edit_form.password, assigns(:email_credential).hashed_password))
+  end
+
+  test "POST update_password, invalid form" do
+    @edit_form.password = "x"
+    assert_equal(false, @edit_form.valid?)
+
+    post :update_password, :email_credential_id => @yuya_gmail.id, :edit_form => @edit_form.attributes
+
+    assert_response(:success)
+    assert_template("edit_password")
+    assert_flash_error
+
+    assert_equal(nil, assigns(:edit_form).password)
+    assert_equal(nil, assigns(:edit_form).password_confirmation)
+  end
+
+  test "GET update_password, abnormal, method not allowed" do
+    get :update_password
+
+    assert_response(405)
+    assert_template(nil)
+  end
+
+  test "POST update_password, abnormal, no login" do
+    session_logout
+
+    post :update_password
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "POST update_password, abnormal, no email credential id" do
+    post :update_password, :email_credential_id => nil
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "POST update_password, abnormal, other's email credential" do
+    post :update_password, :email_credential_id => @risa_example.id
 
     assert_response(:redirect)
     assert_redirected_to(root_path)
