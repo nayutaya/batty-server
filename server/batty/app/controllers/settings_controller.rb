8b47c3996bbbd7644398454fa813f42fcc647772
Yuya Kato <yuyakato@gmail.com>
2009-05-26 22:36:31 +0900

    ニックネームの取得処理のテストを追加

--- server/batty/app/controllers/settings_controller.rb
+++ server/batty/app/controllers/settings_controller.rb
@@ -1,19 +1,27 @@
 
 # ユーザ設定
 class SettingsController < ApplicationController
-  before_filter :authentication
-  before_filter :authentication_required
+  verify(
+    :xhr    => true,
+    :render => {:text => "Method Not Allowed", :status => 405},
+    :only   => [:get_nickname])
+  before_filter :authentication, :except => [:set_nickname]
+  before_filter :authentication_required, :except => [:set_nickname]
 
   # GET /settings
   def index
     # nop
   end
 
+  # XHR GET /settings/get_nickname
   def get_nickname
     render(:text => @login_user.nickname)
   end
 
+  # ?
   def set_nickname
+    render(:text => "")
+    return
     @login_user.nickname = params[:value]
     if @login_user.save
       render(:text => ERB::Util.h(@login_user.nickname))
--- server/batty/test/functional/settings_controller_test.rb
+++ server/batty/test/functional/settings_controller_test.rb
@@ -11,7 +11,9 @@ class SettingsControllerTest < ActionController::TestCase
   test "routes" do
     base = {:controller => "settings"}
 
-    assert_routing("/settings", base.merge(:action => "index"))
+    assert_routing("/settings",              base.merge(:action => "index"))
+    assert_routing("/settings/get_nickname", base.merge(:action => "get_nickname"))
+    assert_routing("/settings/set_nickname", base.merge(:action => "set_nickname"))
   end
 
   test "GET index" do
@@ -27,6 +29,46 @@ class SettingsControllerTest < ActionController::TestCase
     session_logout
 
     get :index
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "XHR GET get_nickname" do
+    xhr :get, :get_nickname
+
+    assert_response(:success)
+    assert_template(nil)
+    assert_flash_empty
+    assert_logged_in(@yuya)
+
+    assert_equal(@yuya.nickname, @response.body)
+  end
+
+  test "XHR GET get_nickname, html" do
+    @yuya.update_attributes!(:nickname => "<&>")
+
+    xhr :get, :get_nickname
+
+    assert_response(:success)
+    assert_template(nil)
+    assert_flash_empty
+
+    assert_equal("<&>", @response.body)
+  end
+
+  test "GET get_nickname, abnormal, method not allowed" do
+    get :get_nickname
+
+    assert_response(405)
+    assert_template(nil)
+  end
+
+  test "XHR GET get_nickname, abnormal, no login" do
+    session_logout
+
+    xhr :get, :get_nickname
 
     assert_response(:redirect)
     assert_redirected_to(root_path)
