15ff7ed9d5d7dabd4580e43db7d70013dc435077
Yuya Kato <yuyakato@gmail.com>
2009-05-26 22:09:12 +0900

    ニックネームの編集に検証エラーの処理を追加

--- server/batty/app/controllers/settings_controller.rb
+++ server/batty/app/controllers/settings_controller.rb
@@ -1,12 +1,26 @@
 
 # ユーザ設定
 class SettingsController < ApplicationController
-  in_place_edit_for :user, :nickname
   before_filter :authentication
   before_filter :authentication_required
 
   # GET /settings
   def index
     @user = @login_user
+  end
+
+  def get_nickname
+    render(:text => @login_user.nickname)
+  end
+
+  def set_nickname
+    @login_user.nickname = params[:value]
+    if @login_user.save
+      render(:text => @login_user.nickname)
+    else
+      message = @login_user.errors.on(:nickname)
+      message = message.join(", ") if message.kind_of?(Array)
+      render(:text => message, :status => 400)
+    end
   end
 end
