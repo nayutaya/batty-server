e6a6ef0b77ec7ebe00c87b93619d8c8ca1852cec
Yuya Kato <yuyakato@gmail.com>
2009-05-26 19:30:56 +0900

    ニックネームの編集を試験的に実装

--- server/batty/app/controllers/settings_controller.rb
+++ server/batty/app/controllers/settings_controller.rb
@@ -1,11 +1,12 @@
 
 # ユーザ設定
 class SettingsController < ApplicationController
+  in_place_edit_for :user, :nickname
   before_filter :authentication
   before_filter :authentication_required
 
   # GET /settings
   def index
-    # nop
+    @user = @login_user
   end
 end
