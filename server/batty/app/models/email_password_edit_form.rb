219ecdfc126daccf336e14cb6e4f40a624ea0fb3
Yuya Kato <yuyakato@gmail.com>
2009-05-26 23:53:52 +0900

    メールパスワード編集フォームに検証を追加

--- server/batty/app/models/email_password_edit_form.rb
+++ server/batty/app/models/email_password_edit_form.rb
@@ -3,4 +3,17 @@
 class EmailPasswordEditForm < ActiveForm
   column :password,              :type => :text
   column :password_confirmation, :type => :text
+
+  validates_presence_of :password
+  validates_presence_of :password_confirmation
+  validates_length_of :password, :in => 4..20, :allow_nil => true
+  validates_format_of :password, :with => /\A[\x21-\x7E]+\z/, :allow_nil => true
+  validates_each(:password) { |record, attr, value|
+    # MEMO: validates_confirmation_ofはpassword_confirmation属性を上書きしてしまうため、
+    #       ここでは使用できない。そのため、validates_confirmation_ofを参考に独自に実装。
+    confirmation = record.__send__("#{attr}_confirmation")
+    if confirmation.blank? || value != confirmation
+      record.errors.add(attr, :confirmation)
+    end
+  }
 end
--- server/batty/test/unit/email_password_edit_form_test.rb
+++ server/batty/test/unit/email_password_edit_form_test.rb
@@ -4,6 +4,9 @@ require 'test_helper'
 class EmailPasswordEditFormTest < ActiveSupport::TestCase
   def setup
     @klass = EmailPasswordEditForm
+    @basic = @klass.new(
+      :password              => "password",
+      :password_confirmation => "password")
   end
 
   #
@@ -28,5 +31,67 @@ class EmailPasswordEditFormTest < ActiveSupport::TestCase
       form.__send__("#{name}=", set_value)
       assert_equal(get_value, form.__send__(name), name)
     }
+  end
+
+  #
+  # 検証
+  #
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  test "validates_presence_of :password" do
+    @basic.password = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :password_confirmation" do
+    @basic.password_confirmation = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_length_of :password" do
+    [
+      ["a" *  3, false],
+      ["a" *  4, true ],
+      ["a" * 20, true ],
+      ["a" * 21, false],
+    ].each { |value, expected|
+      @basic.password              = value
+      @basic.password_confirmation = value
+      assert_equal(expected, @basic.valid?, value)
+    }
+  end
+
+  test "validates_format_of :password" do
+    valid_chars = (0x21..0x7E).map { |c| c.chr }.join
+
+    [
+      [valid_chars.slice!(0, 20), true ],
+      [valid_chars.slice!(0, 20), true ],
+      [valid_chars.slice!(0, 20), true ],
+      [valid_chars.slice!(0, 20), true ],
+      [valid_chars.slice!(0, 20), true ],
+      ["aaaa",   true ],
+      ["aaa ",   false],
+      ["日本語", false],
+    ].each { |value, expected|
+      @basic.password              = value
+      @basic.password_confirmation = value
+      assert_equal(expected, @basic.valid?, value)
+    }
+
+    assert_equal(true, valid_chars.empty?)
+  end
+
+  test "validates_each, password" do
+    @basic.password              = "aaaa"
+    @basic.password_confirmation = "aaaa"
+    assert_equal(true, @basic.valid?)
+
+    @basic.password              = "aaaa"
+    @basic.password_confirmation = "AAAA"
+    assert_equal(false, @basic.valid?)
   end
 end
