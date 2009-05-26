477c65e6fbd27d9b291ee5679f323b2867194d40
Yuya Kato <yuyakato@gmail.com>
2009-05-26 23:42:03 +0900

    パスワードの変更画面を追加

--- server/batty/app/controllers/credentials/email_controller.rb
+++ server/batty/app/controllers/credentials/email_controller.rb
@@ -1,2 +1,39 @@
+
+# メールログイン情報コントローラ
 class Credentials::EmailController < ApplicationController
+  before_filter :authentication
+  before_filter :authentication_required
+  before_filter :required_param_email_credential_id, :only => [:edit_password]
+  before_filter :specified_email_credential_belongs_to_login_user, :only => [:edit_password]
+
+  # GET /credential/email/:email_credential_id/edit_password
+  def edit_password
+    # TODO: 編集用フォームを生成
+  end
+
+  # POST /credential/email/:email_credential_id/update_password
+  # TODO: 実装せよ
+
+  private
+
+  def required_param_email_credential_id(email_credential_id = params[:email_credential_id])
+    @email_credential = EmailCredential.find_by_id(email_credential_id)
+    if @email_credential
+      return true
+    else
+      set_error("メールログイン情報IDが正しくありません。")
+      redirect_to(root_path)
+      return false
+    end
+  end
+
+  def specified_email_credential_belongs_to_login_user
+    if @email_credential.user_id == @login_user.id
+      return true
+    else
+      set_error("メールログイン情報IDが正しくありません。")
+      redirect_to(root_path)
+      return false
+    end
+  end
 end
--- server/batty/test/functional/credentials/email_controller_test.rb
+++ server/batty/test/functional/credentials/email_controller_test.rb
@@ -2,10 +2,57 @@
 require 'test_helper'
 
 class Credentials::EmailControllerTest < ActionController::TestCase
+  def setup
+    @yuya         = users(:yuya)
+    @yuya_gmail   = email_credentials(:yuya_gmail)
+    @risa_example = email_credentials(:risa_example)
+
+    session_login(@yuya)
+  end
+
   test "routes" do
     base = {:controller => "credentials/email"}
 
     assert_routing("/credential/email/1234567890/edit_password",   base.merge(:action => "edit_password",   :email_credential_id => "1234567890"))
     assert_routing("/credential/email/1234567890/update_password", base.merge(:action => "update_password", :email_credential_id => "1234567890"))
+  end
+
+  test "GET edit_password" do
+    get :edit_password, :email_credential_id => @yuya_gmail.id
+
+    assert_response(:success)
+    assert_template("edit_password")
+    assert_flash_empty
+    assert_logged_in(@yuya)
+
+    assert_equal(@yuya_gmail, assigns(:email_credential))
+
+    # TODO: 編集フォームの処理を実装せよ
+  end
+
+  test "GET edit_password, abnormal, no login" do
+    session_logout
+
+    get :edit_password
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "GET edit_password, abnormal, no email credential id" do
+    get :edit_password, :email_credential_id => nil
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "GET edit_password, abnormal, other's email credential" do
+    get :edit_password, :email_credential_id => @risa_example.id
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
   end
 end
