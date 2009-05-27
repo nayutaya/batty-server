be9a2c480f216dd727210381eccb0e5bfd3c2227
Yuya Kato <yuyakato@gmail.com>
2009-05-28 00:41:11 +0900

    エネルギー履歴画面を追加

--- server/batty/app/controllers/energies_controller.rb
+++ server/batty/app/controllers/energies_controller.rb
@@ -1,5 +1,14 @@
-class EnergiesController < ApplicationController
-  def index
-  end
 
+# エネルギー
+class EnergiesController < ApplicationController
+  before_filter :authentication
+  before_filter :authentication_required
+
+  # GET /energies
+  def index
+    @energies = @login_user.energies.paginate(
+      :order    => "energies.observed_at DESC, energies.id DESC",
+      :page     => params[:page],
+      :per_page => 20)
+  end
 end
--- server/batty/test/functional/energies_controller_test.rb
+++ server/batty/test/functional/energies_controller_test.rb
@@ -1,8 +1,52 @@
+
 require 'test_helper'
 
 class EnergiesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @yuya = users(:yuya)
+
+    session_login(@yuya)
+  end
+  test "routes" do
+    base = {:controller => "energies"}
+
+    assert_routing("/energies", base.merge(:action => "index"))
+  end
+
+  test "GET index" do
+    get :index
+
+    assert_response(:success)
+    assert_template("index")
+    assert_flash_empty
+    assert_logged_in(@yuya)
+
+    assert_equal(@yuya.energies.size, assigns(:energies).total_entries)
+    assert_equal( 1, assigns(:energies).current_page)
+    assert_equal(20, assigns(:energies).per_page)
+    assert_equal(true, assigns(:energies).all? { |e| e.device.user == @yuya })
+    assert_equal(
+      assigns(:energies).sort_by { |e| [e.observed_at, e.id] }.reverse,
+      assigns(:energies))
+  end
+
+  test "GET index, page 2" do
+    get :index, :page => 2
+
+    assert_response(:success)
+    assert_template("index")
+    assert_flash_empty
+
+    assert_equal( 2, assigns(:energies).current_page)
+    assert_equal(20, assigns(:energies).per_page)
+  end
+  test "GET index, abnormal, no login" do
+    session_logout
+
+    get :index
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
   end
 end
