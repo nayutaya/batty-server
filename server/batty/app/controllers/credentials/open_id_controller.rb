c4f6663c03d1196caeecaa17e6ee177a85a4cd6b
Yuya Kato <yuyakato@gmail.com>
2009-05-27 11:10:43 +0900

    OpenIDログイン情報コントローラのルーティングを追加

--- server/batty/app/controllers/credentials/open_id_controller.rb
+++ server/batty/app/controllers/credentials/open_id_controller.rb
@@ -1,2 +1,9 @@
+
+# OpenIDログイン情報コントローラ
 class Credentials::OpenIdController < ApplicationController
+  # GET /credential/open_id/:open_id_credential_id/delete
+  # TODO: 実装せよ
+
+  # POST /credential/open_id/:open_id_credential_id/destroy
+  # TODO: 実装せよ
 end
--- server/batty/test/functional/credentials/open_id_controller_test.rb
+++ server/batty/test/functional/credentials/open_id_controller_test.rb
@@ -1,8 +1,11 @@
+
 require 'test_helper'
 
 class Credentials::OpenIdControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "routes" do
+    base = {:controller => "credentials/open_id"}
+
+    assert_routing("/credential/open_id/1234567890/delete",  base.merge(:action => "delete",  :open_id_credential_id => "1234567890"))
+    assert_routing("/credential/open_id/1234567890/destroy", base.merge(:action => "destroy", :open_id_credential_id => "1234567890"))
   end
 end
