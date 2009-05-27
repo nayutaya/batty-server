b9d36f1443bce08015e11795b3a180895fa63167
Yuya Kato <yuyakato@gmail.com>
2009-05-27 11:21:20 +0900

    OpenIDログイン情報コントローラに削除画面を追加

--- server/batty/app/controllers/credentials/open_id_controller.rb
+++ server/batty/app/controllers/credentials/open_id_controller.rb
@@ -1,9 +1,39 @@
 
 # OpenIDログイン情報コントローラ
 class Credentials::OpenIdController < ApplicationController
+  before_filter :authentication
+  before_filter :authentication_required
+  before_filter :required_param_open_id_credential_id
+  before_filter :specified_open_id_credential_belongs_to_login_user
+
   # GET /credential/open_id/:open_id_credential_id/delete
-  # TODO: 実装せよ
+  def delete
+    # nop
+  end
 
   # POST /credential/open_id/:open_id_credential_id/destroy
   # TODO: 実装せよ
+
+  private
+
+  def required_param_open_id_credential_id(open_id_credential_id = params[:open_id_credential_id])
+    @open_id_credential = OpenIdCredential.find_by_id(open_id_credential_id)
+    if @open_id_credential
+      return true
+    else
+      set_error("OpenIDログイン情報IDが正しくありません。")
+      redirect_to(root_path)
+      return false
+    end
+  end
+
+  def specified_open_id_credential_belongs_to_login_user
+    if @open_id_credential.user_id == @login_user.id
+      return true
+    else
+      set_error("OpenIDログイン情報IDが正しくありません。")
+      redirect_to(root_path)
+      return false
+    end
+  end
 end
--- server/batty/test/functional/credentials/open_id_controller_test.rb
+++ server/batty/test/functional/credentials/open_id_controller_test.rb
@@ -2,10 +2,55 @@
 require 'test_helper'
 
 class Credentials::OpenIdControllerTest < ActionController::TestCase
+  def setup
+    @yuya           = users(:yuya)
+    @yuya_livedoor  = open_id_credentials(:yuya_livedoor)
+    @shinya_example = open_id_credentials(:shinya_example)
+
+    session_login(@yuya)
+  end
+
   test "routes" do
     base = {:controller => "credentials/open_id"}
 
     assert_routing("/credential/open_id/1234567890/delete",  base.merge(:action => "delete",  :open_id_credential_id => "1234567890"))
     assert_routing("/credential/open_id/1234567890/destroy", base.merge(:action => "destroy", :open_id_credential_id => "1234567890"))
+  end
+
+  test "GET delete" do
+    get :delete, :open_id_credential_id => @yuya_livedoor.id
+
+    assert_response(:success)
+    assert_template("delete")
+    assert_flash_empty
+    assert_logged_in(@yuya)
+
+    assert_equal(@yuya_livedoor, assigns(:open_id_credential))
+  end
+
+  test "GET delete, abnormal, no login" do
+    session_logout
+
+    get :delete
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "GET delete, abnormal, no openid credential id" do
+    get :delete, :open_id_credential_id => nil
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
+  end
+
+  test "GET delete, abnormal, other's openid credential" do
+    get :delete, :open_id_credential_id => @shinya_example.id
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
   end
 end
