1c81f6b4c075e799837ee1a448604dec9ad2ec2b
Yuya Kato <yuyakato@gmail.com>
2009-05-28 19:47:58 +0900

    日付のフォーマット処理を追加

--- server/batty/lib/notice_formatter.rb
+++ server/batty/lib/notice_formatter.rb
@@ -1,3 +1,12 @@
 
 module NoticeFormatter
+  def self.format_date(date)
+    return {
+      "date"    => date.strftime("%Y-%m-%d"),
+      "date:ja" => date.strftime("%Y年%m月%d日"),
+      "yyyy"    => date.strftime("%Y"),
+      "mm"      => date.strftime("%m"),
+      "dd"      => date.strftime("%d"),
+    }
+  end
 end
--- server/batty/test/unit/notice_formatter_test.rb
+++ server/batty/test/unit/notice_formatter_test.rb
@@ -2,4 +2,18 @@
 require 'test_helper'
 
 class NoticeFormatterTest < ActiveSupport::TestCase
+  def setup
+    @module = NoticeFormatter
+  end
+
+  test "format_date" do
+    expected = {
+      "date"    => "2009-01-02",
+      "date:ja" => "2009年01月02日",
+      "yyyy"    => "2009",
+      "mm"      => "01",
+      "dd"      => "02",
+    }
+    assert_equal(expected, @module.format_date(Date.new(2009, 1, 2)))
+  end
 end
