7f979ec780c0fe8cba4b26981cdc3ef4e0732613
Yuya Kato <yuyakato@gmail.com>
2009-05-28 14:25:16 +0900

    HTTPアクション実行クラスに属性と実行メソッドを追加

--- server/batty/lib/http_action_executor.rb
+++ server/batty/lib/http_action_executor.rb
@@ -3,9 +3,34 @@
 class HttpActionExecutor
   def initialize(options = {})
     options = options.dup
-    @url         = options.delete(:url)
-    @http_method = options.delete(:http_method)
-    @post_body   = options.delete(:post_body)
+    @url         = options.delete(:url)         || nil
+    @http_method = options.delete(:http_method) || nil
+    @post_body   = options.delete(:post_body)   || nil
     raise(ArgumentError) unless options.empty?
+  end
+
+  attr_accessor :url, :http_method, :post_body
+
+  def execute
+    case @http_method
+    when :head then execute_by_head
+    when :get  then execute_by_get
+    when :post then execute_by_post
+    else raise("invalid http method")
+    end
+  end
+
+  private
+
+  def execute_by_head
+    # nop
+  end
+
+  def execute_by_get
+    # nop
+  end
+
+  def execute_by_post
+    # nop
   end
 end
--- server/batty/test/unit/http_action_executor_test.rb
+++ server/batty/test/unit/http_action_executor_test.rb
@@ -3,8 +3,14 @@ require 'test_helper'
 
 class HttpActionExecutorTest < ActiveSupport::TestCase
   def setup
-    @klass = HttpActionExecutor
+    @klass    = HttpActionExecutor
+    @executor = @klass.new
+    @musha    = Kagemusha.new(@klass)
   end
+
+  #
+  # 初期化
+  #
 
   test "initialize" do
     executor = @klass.new(
@@ -19,6 +25,51 @@ class HttpActionExecutorTest < ActiveSupport::TestCase
   test "initialize, invalid parameter" do
     assert_raise(ArgumentError) {
       @klass.new(:invalid => true)
+    }
+  end
+
+  #
+  # 属性
+  #
+
+  test "attributes" do
+    [
+      [:url,         nil, "str", "str"],
+      [:http_method, nil, :sym, :sym],
+      [:post_body,   nil, "str", "str"],
+    ].each { |name, default, set_value, get_value|
+      executor = @klass.new
+      assert_equal(default, executor.__send__(name), name)
+      executor.__send__("#{name}=", set_value)
+      assert_equal(get_value, executor.__send__(name), name)
+    }
+  end
+
+  #
+  # インスタンスメソッド
+  #
+
+  test "execute" do
+    called = nil
+    @musha.def(:execute_by_head) { called = :head }
+    @musha.def(:execute_by_get)  { called = :get  }
+    @musha.def(:execute_by_post) { called = :post }
+
+    @executor.http_method = :head
+    @musha.swap { @executor.execute }
+    assert_equal(:head, called)
+
+    @executor.http_method = :get
+    @musha.swap { @executor.execute }
+    assert_equal(:get, called)
+
+    @executor.http_method = :post
+    @musha.swap { @executor.execute }
+    assert_equal(:post, called)
+
+    @executor.http_method = :invalid
+    assert_raise(RuntimeError) {
+      @musha.swap { @executor.execute }
     }
   end
 end
