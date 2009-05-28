00382101c0c3a837d17ba51b7c49282f6177b656
Yuya Kato <yuyakato@gmail.com>
2009-05-28 20:19:45 +0900

    JSON形式の出力を追加

--- server/batty/lib/notice_formatter.rb
+++ server/batty/lib/notice_formatter.rb
@@ -2,28 +2,31 @@
 module NoticeFormatter
   def self.format_date(date)
     return {
-      "date"    => date.strftime("%Y-%m-%d"),
-      "date:ja" => date.strftime("%Y年%m月%d日"),
-      "yyyy"    => date.strftime("%Y"),
-      "mm"      => date.strftime("%m"),
-      "dd"      => date.strftime("%d"),
+      "date"      => date.try(:strftime, "%Y-%m-%d")     || "-",
+      "date:json" => date.try(:strftime, '"%Y-%m-%d"')   || "null",
+      "date:ja"   => date.try(:strftime, "%Y年%m月%d日") || "-",
+      "yyyy"      => date.try(:strftime, "%Y")           || "-",
+      "mm"        => date.try(:strftime, "%m")           || "-",
+      "dd"        => date.try(:strftime, "%d")           || "-",
     }
   end
 
   def self.format_time(time)
     return {
-      "time"    => time.strftime("%H:%M:%S"),
-      "time:ja" => time.strftime("%H時%M分%S秒"),
-      "hh"      => time.strftime("%H"),
-      "nn"      => time.strftime("%M"),
-      "ss"      => time.strftime("%S"),
+      "time"      => time.try(:strftime, "%H:%M:%S")     || "-",
+      "time:json" => time.try(:strftime, '"%H:%M:%S"')   || "null",
+      "time:ja"   => time.try(:strftime, "%H時%M分%S秒") || "-",
+      "hh"        => time.try(:strftime, "%H")           || "-",
+      "nn"        => time.try(:strftime, "%M")           || "-",
+      "ss"        => time.try(:strftime, "%S")           || "-",
     }
   end
 
   def self.format_datetime(datetime)
     result = {
-      "datetime"    => datetime.strftime("%Y-%m-%d %H:%M:%S"),
-      "datetime:ja" => datetime.strftime("%Y年%m月%d日 %H時%M分%S秒"),
+      "datetime"      => datetime.try(:strftime, "%Y-%m-%d %H:%M:%S")         || "-",
+      "datetime:json" => datetime.try(:strftime, '"%Y-%m-%d %H:%M:%S"')       || "null",
+      "datetime:ja"   => datetime.try(:strftime, "%Y年%m月%d日 %H時%M分%S秒") || "-",
     }
     result.merge!(self.format_date(datetime))
     result.merge!(self.format_time(datetime))
--- server/batty/test/unit/notice_formatter_test.rb
+++ server/batty/test/unit/notice_formatter_test.rb
@@ -8,34 +8,72 @@ class NoticeFormatterTest < ActiveSupport::TestCase
 
   test "format_date" do
     expected = {
-      "date"    => "2009-01-02",
-      "date:ja" => "2009年01月02日",
-      "yyyy"    => "2009",
-      "mm"      => "01",
-      "dd"      => "02",
+      "date"      => "2009-01-02",
+      "date:json" => '"2009-01-02"',
+      "date:ja"   => "2009年01月02日",
+      "yyyy"      => "2009",
+      "mm"        => "01",
+      "dd"        => "02",
     }
     assert_equal(expected, @module.format_date(Date.new(2009, 1, 2)))
   end
 
+  test "format_date, nil" do
+    expected = {
+      "date"      => "-",
+      "date:json" => "null",
+      "date:ja"   => "-",
+      "yyyy"      => "-",
+      "mm"        => "-",
+      "dd"        => "-",
+    }
+    assert_equal(expected, @module.format_date(nil))
+  end
+
   test "format_time" do
     expected = {
-      "time"    => "01:02:03",
-      "time:ja" => "01時02分03秒",
-      "hh"      => "01",
-      "nn"      => "02",
-      "ss"      => "03",
+      "time"      => "01:02:03",
+      "time:json" => '"01:02:03"',
+      "time:ja"   => "01時02分03秒",
+      "hh"        => "01",
+      "nn"        => "02",
+      "ss"        => "03",
     }
     assert_equal(expected, @module.format_time(Time.local(2009, 12, 31, 1, 2, 3)))
+  end
+
+  test "format_time, nil" do
+    expected = {
+      "time"      => "-",
+      "time:json" => "null",
+      "time:ja"   => "-",
+      "hh"        => "-",
+      "nn"        => "-",
+      "ss"        => "-",
+    }
+    assert_equal(expected, @module.format_time(nil))
   end
 
   test "format_datetime" do
     datetime = Time.local(2009, 12, 31, 12, 34, 56)
     expected = {
-      "datetime"    => "2009-12-31 12:34:56",
-      "datetime:ja" => "2009年12月31日 12時34分56秒",
+      "datetime"      => "2009-12-31 12:34:56",
+      "datetime:json" => '"2009-12-31 12:34:56"',
+      "datetime:ja"   => "2009年12月31日 12時34分56秒",
     }
     expected.merge!(@module.format_date(datetime))
     expected.merge!(@module.format_time(datetime))
     assert_equal(expected, @module.format_datetime(datetime))
+  end
+
+  test "format_datetime, nil" do
+    expected = {
+      "datetime"      => "-",
+      "datetime:json" => "null",
+      "datetime:ja"   => "-",
+    }
+    expected.merge!(@module.format_date(nil))
+    expected.merge!(@module.format_time(nil))
+    assert_equal(expected, @module.format_datetime(nil))
   end
 end
