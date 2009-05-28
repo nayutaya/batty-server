f004da616c6184187870cb61d6da2bd4ee758eb9
Yuya Kato <yuyakato@gmail.com>
2009-05-28 14:03:42 +0900

    HTTPアクション実行クラスに初期化処理を追加

--- server/batty/lib/http_action_executor.rb
+++ server/batty/lib/http_action_executor.rb
@@ -1,5 +1,11 @@
 
 # HTTPアクション実行
 class HttpActionExecutor
-
+  def initialize(options = {})
+    options = options.dup
+    @url         = options.delete(:url)
+    @http_method = options.delete(:http_method)
+    @post_body   = options.delete(:post_body)
+    raise(ArgumentError) unless options.empty?
+  end
 end
--- server/batty/test/unit/http_action_executor_test.rb
+++ server/batty/test/unit/http_action_executor_test.rb
@@ -5,4 +5,20 @@ class HttpActionExecutorTest < ActiveSupport::TestCase
   def setup
     @klass = HttpActionExecutor
   end
+
+  test "initialize" do
+    executor = @klass.new(
+      :url         => "http://example.jp/",
+      :http_method => :get,
+      :post_body   => "body")
+    assert_equal("http://example.jp/", executor.instance_variable_get("@url"))
+    assert_equal(:get,                 executor.instance_variable_get("@http_method"))
+    assert_equal("body",               executor.instance_variable_get("@post_body"))
+  end
+
+  test "initialize, invalid parameter" do
+    assert_raise(ArgumentError) {
+      @klass.new(:invalid => true)
+    }
+  end
 end
