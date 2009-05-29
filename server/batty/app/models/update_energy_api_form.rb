95eabe88bd18f9a4f2404ad8b7425c4bf66f5a0c
Yuya Kato <yuyakato@gmail.com>
2009-05-29 10:56:55 +0900

    エネルギー更新APIフォームに検証を追加

--- server/batty/app/models/update_energy_api_form.rb
+++ server/batty/app/models/update_energy_api_form.rb
@@ -3,4 +3,19 @@
 class UpdateEnergyApiForm < ActiveForm
   column :level, :type => :integer
   column :time,  :type => :integer
+
+  # MEMO: 符号つき32ビット整数で表せる時間の範囲
+  TimeMinimumValue = Time.at(0x00000000).strftime("%Y%m%d%H%M%S").to_i
+  TimeMaximumValue = Time.at(0x7FFFFFFF).strftime("%Y%m%d%H%M%S").to_i
+
+  validates_presence_of :level
+  validates_presence_of :time
+  validates_numericality_of :level, :only_integer => true, :allow_nil => true
+  validates_numericality_of :time, :only_integer => true, :allow_nil => true
+  validates_inclusion_of :level, :in => Energy::LevelRange, :allow_nil => true
+  validates_inclusion_of :time, :in => TimeMinimumValue..TimeMaximumValue, :allow_nil => true
+  # TODO: timeの時間としての正しさ
+
+  # TODO: paramsからインスタンスを生成するメソッドを追加
+  # TODO: Energyモデルを作成するためのハッシュを生成するメソッドを追加
 end
--- server/batty/test/unit/update_energy_api_form_test.rb
+++ server/batty/test/unit/update_energy_api_form_test.rb
@@ -4,6 +4,9 @@ require 'test_helper'
 class UpdateEnergyApiFormTest < ActiveSupport::TestCase
   def setup
     @klass = UpdateEnergyApiForm
+    @basic = @klass.new(
+      :level => "100",
+      :time  => "20090101000000")
   end
 
   #
@@ -27,6 +30,64 @@ class UpdateEnergyApiFormTest < ActiveSupport::TestCase
       assert_equal(default, form.__send__(name), name)
       form.__send__("#{name}=", set_value)
       assert_equal(get_value, form.__send__(name), name)
+    }
+  end
+
+  #
+  # 検証
+  #
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  test "validates_presence_of :level" do
+    @basic.level = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_presence_of :time" do
+    @basic.time = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_numericality_of :level" do
+    @basic.level = "x"
+    assert_equal(false, @basic.valid?)
+
+    @basic.level = "0.1"
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_numericality_of :time" do
+    @basic.time = "x"
+    assert_equal(false, @basic.valid?)
+
+    @basic.time = "0.1"
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_inclusion_of :level" do
+    [
+      [ -1, false],
+      [  0, true ],
+      [100, true ],
+      [101, false],
+    ].each { |value, expected|
+      @basic.level = value
+      assert_equal(expected, @basic.valid?, value)
+    }
+  end
+
+  test "validates_inclusion_of :time" do
+    [
+      [@klass::TimeMinimumValue - 1, false],
+      [@klass::TimeMinimumValue,     true ],
+      [@klass::TimeMaximumValue,     true ],
+      [@klass::TimeMaximumValue + 1, false],
+    ].each { |value, expected|
+      @basic.time = value
+      assert_equal(expected, @basic.valid?, value)
     }
   end
 end
