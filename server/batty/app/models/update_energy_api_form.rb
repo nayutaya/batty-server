254cba613207ba9eb05e8f09fee08dda9dfd1dc7
Yuya Kato <yuyakato@gmail.com>
2009-05-29 10:24:23 +0900

    エネルギー更新APIフォームにカラムを追加

--- server/batty/app/models/update_energy_api_form.rb
+++ server/batty/app/models/update_energy_api_form.rb
@@ -1,2 +1,6 @@
-class UpdateEnergyApiForm < ActiveRecord::Base
+
+# エネルギー更新APIフォーム
+class UpdateEnergyApiForm < ActiveForm
+  column :level, :type => :integer
+  column :time,  :type => :integer
 end
