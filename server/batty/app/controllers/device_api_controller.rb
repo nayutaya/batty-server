2f83aa241e55bea4f8b1c273b45798a7b9d666f9
Yuya Kato <yuyakato@gmail.com>
2009-05-29 13:47:01 +0900

    試験的に実装したメールアクション実行処理を削除

--- server/batty/app/controllers/device_api_controller.rb
+++ server/batty/app/controllers/device_api_controller.rb
@@ -19,36 +19,6 @@ class DeviceApiController < ApplicationController
 
     records = @device.update_energy(@api_form.to_energy_hash.merge(:update_event => true))
 
-    # TODO: テストを記述
-    @email_actions = []
-    records.each { |record|
-      trigger = record[:trigger]
-      @email_actions += trigger.email_actions.enable.map { |email_action| record.dup.merge(:email_action => email_action) }
-      # TODO: HttpAction
-    }
-
-    # TODO: テストを記述
-    @notify_mails = @email_actions.map { |info|
-      email_action = info[:email_action]
-      {
-        :subject    => email_action.subject,
-        :recipients => "mr.yu-yan.free@docomo.ne.jp",
-        :body       => email_action.body,
-      }
-    }
-
-    # TODO: テストを記述
-    # TODO: 非同期化
-    begin
-      @notify_mails.each { |notify_mail|
-        EventNotification.deliver_notify(notify_mail)
-      }
-    rescue Exception => e
-      logger.add(
-        logger.class::ERROR,
-        format("%s\n%s: %s\n%s\n%s", "-" * 50, e.class.name, e.message, e.backtrace[0, 5].join("\n"), "-" * 50))
-    end
-
     render(:text => "success")
   end
 
