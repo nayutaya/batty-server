84baff7e7f41db14d82f02103bfa39d363744ba2
Yuya Kato <yuyakato@gmail.com>
2009-06-01 13:42:42 +0900

    メールアドレス追加画面を追加

--- server/batty/app/controllers/emails_controller.rb
+++ server/batty/app/controllers/emails_controller.rb
@@ -1,2 +1,16 @@
+
+# メールアドレス
 class EmailsController < ApplicationController
+  EditFormClass = EmailAddressEditForm
+
+  before_filter :authentication
+  before_filter :authentication_required
+
+  # GET /emails/new
+  def new
+    @edit_form = EditFormClass.new
+  end
+
+  # POST /emails/create
+  # TODO: 実装せよ
 end
--- server/batty/test/functional/emails_controller_test.rb
+++ server/batty/test/functional/emails_controller_test.rb
@@ -1,8 +1,40 @@
+
 require 'test_helper'
 
 class EmailsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @yuya = users(:yuya)
+
+    session_login(@yuya)
+  end
+
+  test "routes" do
+    base = {:controller => "emails"}
+
+    assert_routing("/emails/new",    base.merge(:action => "new"))
+    assert_routing("/emails/create", base.merge(:action => "create"))
+  end
+
+  test "GET new" do
+    get :new
+
+    assert_response(:success)
+    assert_template("new")
+    assert_flash_empty
+    assert_logged_in(@yuya)
+
+    assert_equal(
+      EmailAddressEditForm.new.attributes,
+      assigns(:edit_form).attributes)
+  end
+
+  test "GET new, abnormal, no login" do
+    session_logout
+
+    get :new
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+    assert_flash_error
   end
 end
