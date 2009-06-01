c0de635ed9abd8d9837fd3a702107fbe73e5dd40
Yuya Kato <yuyakato@gmail.com>
2009-06-01 12:04:59 +0900

    メールアドレス編集フォームに検証を追加

--- server/batty/app/models/email_address_edit_form.rb
+++ server/batty/app/models/email_address_edit_form.rb
@@ -2,4 +2,8 @@
 # メールアドレス編集フォーム
 class EmailAddressEditForm < ActiveForm
   column :email, :type => :text
+
+  validates_presence_of :email
+  validates_length_of :email, :maximum => 200, :allow_nil => true
+  validates_email_format_of :email
 end
--- server/batty/test/unit/email_address_edit_form_test.rb
+++ server/batty/test/unit/email_address_edit_form_test.rb
@@ -4,6 +4,8 @@ require 'test_helper'
 class EmailAddressEditFormTest < ActiveSupport::TestCase
   def setup
     @klass = EmailAddressEditForm
+    @basic = @klass.new(
+      :email => "email@example.jp")
   end
 
   #
@@ -26,6 +28,47 @@ class EmailAddressEditFormTest < ActiveSupport::TestCase
       assert_equal(default, form.__send__(name), name)
       form.__send__("#{name}=", set_value)
       assert_equal(get_value, form.__send__(name), name)
+    }
+  end
+
+  #
+  # 検証
+  #
+
+  test "basic is valid" do
+    assert_equal(true, @basic.valid?)
+  end
+
+  test "validates_presence_of :email" do
+    @basic.email = nil
+    assert_equal(false, @basic.valid?)
+  end
+
+  test "validates_length_of :email" do
+    # MEMO: 下記の制約を満たしつつ、文字列長の検証のテストを行う
+    #       * ローカルパートは64文字以内
+    #       * ドメインパートは255文字以内
+    #       * ドメインパートのドットで区切られたパートは63文字以内
+
+    [
+      ["a@b.c.d.com", 11, true ],
+      [["a" * 64 + "@" + "b" * 63, "c" * 63, "d" * 3, "com"].join("."), 200, true ],
+      [["a" * 64 + "@" + "b" * 63, "c" * 63, "d" * 4, "com"].join("."), 201, false],
+    ].each { |value, length, expected|
+      assert_equal(length, value.size, value)
+      @basic.email = value
+      assert_equal(expected, @basic.valid?, value)
+    }
+  end
+
+  test "validates_email_format_of :email" do
+    [
+      ["foo@example.com",   true ],
+      ["foo@example.co.jp", true ],
+      ["foo@example",       false],
+    ].each { |value, expected|
+      @basic.email = value
+      assert_equal(expected, @basic.valid?, value)
     }
   end
 end
