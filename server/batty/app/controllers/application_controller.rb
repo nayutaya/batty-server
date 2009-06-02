857075ed38b5264f0a34c30ec2ff9e56904634c8
Yuya Kato <yuyakato@gmail.com>
2009-06-02 12:01:42 +0900

    リファクタリング
    * C0カバレッジを把握しやすいようにコーディングスタイルを変更

--- server/batty/app/controllers/application_controller.rb
+++ server/batty/app/controllers/application_controller.rb
@@ -21,36 +21,39 @@ class ApplicationController < ActionController::Base
   end
 
   def authentication_required
-    return true if @login_user
-
-    set_error("ログインが必要です。")
-    redirect_to(root_path)
-
-    return false
+    if @login_user
+      return true
+    else
+      set_error("ログインが必要です。")
+      redirect_to(root_path)
+      return false
+    end
   end
 
   def required_param_device_token(device_token = params[:device_token])
     @device = Device.find_by_device_token(device_token)
-    return true if @device
-
-    if block_given?
-      yield
+    if @device
+      return true
     else
-      set_error("デバイストークンが正しくありません。")
-      redirect_to(root_path)
+      if block_given?
+        yield
+      else
+        set_error("デバイストークンが正しくありません。")
+        redirect_to(root_path)
+      end
+      return false
     end
-
-    return false
   end
 
   def required_param_device_id(device_id = params[:device_id])
     @device = Device.find_by_id(device_id)
-    return true if @device
-
-    set_error("デバイスIDが正しくありません。")
-    redirect_to(root_path)
-
-    return false
+    if @device
+      return true
+    else
+      set_error("デバイスIDが正しくありません。")
+      redirect_to(root_path)
+      return false
+    end
   end
 
   def required_param_trigger_id(trigger_id = params[:trigger_id])
