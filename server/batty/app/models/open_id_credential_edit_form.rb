67bd837be10ef52cdebe1f93a14496c5b680bb79
Yuya Kato <yuyakato@gmail.com>
2009-06-03 10:55:12 +0900

    OpenID認証情報編集フォームにカラムを追加

--- server/batty/app/models/open_id_credential_edit_form.rb
+++ server/batty/app/models/open_id_credential_edit_form.rb
@@ -1,2 +1,5 @@
-class OpenIdCredentialEditForm < ActiveRecord::Base
+
+# OpenID認証情報編集フォーム
+class OpenIdCredentialEditForm < ActiveForm
+  column :openid_url, :type => :text
 end
--- server/batty/test/unit/open_id_credential_edit_form_test.rb
+++ server/batty/test/unit/open_id_credential_edit_form_test.rb
@@ -1,8 +1,31 @@
+
 require 'test_helper'
 
 class OpenIdCredentialEditFormTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @klass = OpenIdCredentialEditForm
+  end
+
+  #
+  # 基底クラス
+  #
+
+  test "superclass" do
+    assert_equal(ActiveForm, @klass.superclass)
+  end
+
+  #
+  # カラム
+  #
+
+  test "columns" do
+    [
+      [:openid_url, nil, "1", "1"],
+    ].each { |name, default, set_value, get_value|
+      form = @klass.new
+      assert_equal(default, form.__send__(name), name)
+      form.__send__("#{name}=", set_value)
+      assert_equal(get_value, form.__send__(name), name)
+    }
   end
 end
