285f146b1dbfb2a0c12c21dcc9eb70d226dba4ad
Yuya Kato <yuyakato@gmail.com>
2009-06-05 13:51:09 +0900

    IPアドレスに基づくアクセス制御を追加

--- server/batty/app/controllers/admin/home_controller.rb
+++ server/batty/app/controllers/admin/home_controller.rb
@@ -1,10 +1,30 @@
 
+require "ipaddr"
+
 # 管理用ホーム
 class Admin::HomeController < ApplicationController
   layout "admin"
+  before_filter :access_control
+
+  AllowedNetworks = [
+    IPAddr.new("0.0.0.0/32"),
+    IPAddr.new("127.0.0.1/32"),
+  ].freeze
 
   # GET /admin
   def index
     # nop
+  end
+
+  private
+
+  def access_control
+    remote_addr = IPAddr.new(request.remote_ip)
+    if AllowedNetworks.any? { |network| network.include?(remote_addr) }
+      return true
+    else
+      redirect_to(root_path)
+      return false
+    end
   end
 end
--- server/batty/test/functional/admin/home_controller_test.rb
+++ server/batty/test/functional/admin/home_controller_test.rb
@@ -7,4 +7,23 @@ class Admin::HomeControllerTest < ActionController::TestCase
 
     assert_routing("/admin", base.merge(:action => "index"))
   end
+
+  test "GET index" do
+    get :index
+
+    assert_response(:success)
+    assert_template("index")
+  end
+
+  test "GET index, deny" do
+    musha = Kagemusha.new(ActionController::TestRequest).
+      def(:remote_ip) { "123.123.123.123" }
+
+    musha.swap {
+      get :index
+    }
+
+    assert_response(:redirect)
+    assert_redirected_to(root_path)
+  end
 end
