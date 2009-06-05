18c576358675b8a17576bc90981a5db273aa7885
Yuya Kato <yuyakato@gmail.com>
2009-06-05 11:58:28 +0900

    Cookieの有効期限を3日間に変更

--- server/batty/config/initializers/session_store.rb
+++ server/batty/config/initializers/session_store.rb
@@ -13,3 +13,4 @@ ActionController::Base.session = {
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
 ActionController::Base.session_store = :active_record_store
+ActionController::Base.session_options[:expire_after] = 3.days
