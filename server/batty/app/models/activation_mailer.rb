9aa8e610945e0088bb80d0ae71268a3f5085e3f8
Yuya Kato <yuyakato@gmail.com>
2009-06-09 16:17:03 +0900

    メールアドレス認証の登録/登録完了メールの生成処理を追加

--- server/batty/app/models/activation_mailer.rb
+++ server/batty/app/models/activation_mailer.rb
@@ -33,6 +33,33 @@ class ActivationMailer < ActionMailer::Base
     }
   end
 
+  def self.create_request_for_credential_params(options)
+    options = options.dup
+    recipients     = options.delete(:recipients)     || raise(ArgumentError)
+    activation_url = options.delete(:activation_url) || raise(ArgumentError)
+    raise(ArgumentError) unless options.empty?
+
+    return {
+      :subject    => SubjectPrefix + "メールアドレス認証登録",
+      :from       => FromAddress,
+      :recipients => recipients,
+      :body       => {:activation_url => activation_url},
+    }
+  end
+
+  def self.create_complete_for_credential_params(options)
+    options = options.dup
+    recipients = options.delete(:recipients) || raise(ArgumentError)
+    raise(ArgumentError) unless options.empty?
+
+    return {
+      :subject    => SubjectPrefix + "メールアドレス認証登録完了",
+      :from       => FromAddress,
+      :recipients => recipients,
+      :body       => {},
+    }
+  end
+
   def self.create_request_for_notice_params(options)
     options = options.dup
     recipients     = options.delete(:recipients)     || raise(ArgumentError)
@@ -66,6 +93,14 @@ class ActivationMailer < ActionMailer::Base
 
   def complete_for_signup(options)
     build_message(self.class.create_complete_for_signup_params(options))
+  end
+
+  def request_for_credential(options)
+    build_message(self.class.create_request_for_credential_params(options))
+  end
+
+  def complete_for_credential(options)
+    build_message(self.class.create_complete_for_credential_params(options))
   end
 
   def request_for_notice(options)
