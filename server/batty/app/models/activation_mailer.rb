15934bfa03796ab3d1f383785abc9ac721c395a4
Yuya Kato <yuyakato@gmail.com>
2009-06-09 11:57:32 +0900

    メッセージ構築の共通処理を追加

--- server/batty/app/models/activation_mailer.rb
+++ server/batty/app/models/activation_mailer.rb
@@ -1,6 +1,7 @@
-class ActivationMailer < ActionMailer::Base
-  
 
+# アクティベーションメーラ
+class ActivationMailer < ActionMailer::Base
+=begin
   def request_for_notice(sent_at = Time.now)
     subject    'ActivationMailer#request_for_notice'
     recipients ''
@@ -9,5 +10,15 @@ class ActivationMailer < ActionMailer::Base
     
     body       :greeting => 'Hi,'
   end
+=end
 
+  def build_message(options)
+    subject(options[:header][:subject])
+    from(options[:header][:from])
+    recipients(options[:header][:recipients])
+    sent_on(Time.now)
+    body(options[:body])
+
+    return nil
+  end
 end
--- server/batty/test/unit/activation_mailer_test.rb
+++ server/batty/test/unit/activation_mailer_test.rb
@@ -1,6 +1,50 @@
+
 require 'test_helper'
 
 class ActivationMailerTest < ActionMailer::TestCase
+  def setup
+    @klass = ActivationMailer
+  end
+
+  #
+  # インスタンスメソッド
+  #
+
+  test "build_message" do
+    time = Time.local(2010, 1, 1)
+    options = {
+      :header => {
+        :subject    => "SUBJECT",
+        :from       => "FROM",
+        :recipients => "RECIPIENTS",
+      },
+      :body => {:KEY => :VALUE},
+    }
+
+    called = []
+    musha = Kagemusha.new(@klass)
+    musha.def(:subject)    { |value| called << [:subject,    value] }
+    musha.def(:from)       { |value| called << [:from,       value] }
+    musha.def(:recipients) { |value| called << [:recipients, value] }
+    musha.def(:sent_on)    { |value| called << [:sent_on,    value] }
+    musha.def(:body)       { |value| called << [:body,       value] }
+    musha.swap {
+      Kagemusha::DateTime.at(time) {
+        assert_equal(nil, @klass.allocate.__send__(:build_message, options))
+      }
+    }
+
+    expected = [
+      [:subject,    "SUBJECT"],
+      [:from,       "FROM"],
+      [:recipients, "RECIPIENTS"],
+      [:sent_on,    time],
+      [:body,       {:KEY => :VALUE}],
+    ]
+    assert_equal(expected, called)
+  end
+
+=begin
   test "request_for_notice" do
     @expected.subject = 'ActivationMailer#request_for_notice'
     @expected.body    = read_fixture('request_for_notice')
@@ -8,5 +52,5 @@ class ActivationMailerTest < ActionMailer::TestCase
 
     assert_equal @expected.encoded, ActivationMailer.create_request_for_notice(@expected.date).encoded
   end
-
+=end
 end
