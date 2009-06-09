203163110fe48fd40711e778797acb0914d1dfd5
Yuya Kato <yuyakato@gmail.com>
2009-06-09 12:14:51 +0900

    アクションメーラユーティリティモジュールに、共通のメッセージ構築処理を追加

--- server/batty/lib/action_mailer_util.rb
+++ server/batty/lib/action_mailer_util.rb
@@ -1,3 +1,14 @@
 
 module ActionMailerUtil
+  private
+
+  def build_message(options)
+    subject(options[:header][:subject])
+    from(options[:header][:from])
+    recipients(options[:header][:recipients])
+    sent_on(Time.now)
+    body(options[:body])
+
+    return nil
+  end
 end
--- server/batty/test/unit/action_mailer_util_test.rb
+++ server/batty/test/unit/action_mailer_util_test.rb
@@ -2,4 +2,42 @@
 require 'test_helper'
 
 class ActionMailerUtilTest < ActiveSupport::TestCase
+  def setup
+    @module   = ActionMailerUtil
+    @instance = Class.new.module_eval { include(ActionMailerUtil); self }.new
+  end
+
+  test "build_message" do
+    time = Time.local(2010, 1, 1)
+    options = {
+      :header => {
+        :subject    => "SUBJECT",
+        :from       => "FROM",
+        :recipients => "RECIPIENTS",
+      },
+      :body => {:KEY => :VALUE},
+    }
+
+    called = []
+    musha = Kagemusha.new(@module)
+    musha.def(:subject)    { |value| called << [:subject,    value] }
+    musha.def(:from)       { |value| called << [:from,       value] }
+    musha.def(:recipients) { |value| called << [:recipients, value] }
+    musha.def(:sent_on)    { |value| called << [:sent_on,    value] }
+    musha.def(:body)       { |value| called << [:body,       value] }
+    musha.swap {
+      Kagemusha::DateTime.at(time) {
+        assert_equal(nil, @instance.__send__(:build_message, options))
+      }
+    }
+
+    expected = [
+      [:subject,    "SUBJECT"],
+      [:from,       "FROM"],
+      [:recipients, "RECIPIENTS"],
+      [:sent_on,    time],
+      [:body,       {:KEY => :VALUE}],
+    ]
+    assert_equal(expected, called)
+  end
 end
