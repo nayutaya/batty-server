aff4c2f396abb09bdaa445798fee7a9ff2cf99b5
Yuya Kato <yuyakato@gmail.com>
2009-06-09 13:44:50 +0900

    通知先メールアドレス登録メールの生成処理を追加

--- server/batty/app/models/activation_mailer.rb
+++ server/batty/app/models/activation_mailer.rb
@@ -3,6 +3,27 @@
 class ActivationMailer < ActionMailer::Base
   include ActionMailerUtil
 
+  SubjectPrefix = "[batty] "
+  FromAddress   = "batty-no-reply@nayutaya.jp"
+
+  def self.create_request_for_notice_params(options)
+    options = options.dup
+    recipients     = options.delete(:recipients)     || raise(ArgumentError)
+    activation_url = options.delete(:activation_url) || raise(ArgumentError)
+    raise(ArgumentError) unless options.empty?
+
+    return {
+      :header => {
+        :subject    => SubjectPrefix + "通知先メールアドレス登録",
+        :from       => FromAddress,
+        :recipients => recipients,
+      },
+      :body   => {
+        :activation_url => activation_url,
+      },
+    }
+  end
+
 =begin
   def request_for_notice(sent_at = Time.now)
     subject    'ActivationMailer#request_for_notice'
--- server/batty/test/unit/activation_mailer_test.rb
+++ server/batty/test/unit/activation_mailer_test.rb
@@ -7,6 +7,46 @@ class ActivationMailerTest < ActionMailer::TestCase
   end
 
   #
+  # クラスメソッド
+  #
+
+  test "self.create_request_for_notice_params" do
+    options = {
+      :recipients     => "recipients@example.jp",
+      :activation_url => "http://activation/url",
+    }
+    expected = {
+      :header => {
+        :subject    => "[batty] 通知先メールアドレス登録",
+        :from       => "batty-no-reply@nayutaya.jp",
+        :recipients => "recipients@example.jp",
+      },
+      :body => {
+        :activation_url => "http://activation/url",
+      },
+    }
+    assert_equal(expected, @klass.create_request_for_notice_params(options))
+  end
+
+  test "self.create_request_for_notice_params, deficient parameter" do
+    assert_nothing_raised {
+      @klass.create_request_for_notice_params(:recipients => "", :activation_url => "")
+    }
+    assert_raise(ArgumentError) {
+      @klass.create_request_for_notice_params(:activation_url => "")
+    }
+    assert_raise(ArgumentError) {
+      @klass.create_request_for_notice_params(:recipients => "")
+    }
+  end
+
+  test "self.create_request_for_notice_params, invalid parameter" do
+    assert_raise(ArgumentError) {
+      @klass.create_request_for_notice_params(:invalid => true)
+    }
+  end
+
+  #
   # インスタンスメソッド
   #
 
