150d2ceada660cff49d0d3c0ef517424db61f7d4
Yuya Kato <yuyakato@gmail.com>
2009-06-10 14:30:32 +0900

    Windows Mobile用クライアントの雛形を追加

---
+++ client/windows_mobile/batty_agent/batty_agent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace batty_agent
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [MTAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+    }
+}
