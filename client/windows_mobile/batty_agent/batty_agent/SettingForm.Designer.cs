48733cb7679e0b5bd15b68b03dc65af769c774ff
Yuya Kato <yuyakato@gmail.com>
2009-06-14 19:49:34 +0900

    設定フォームを追加

---
+++ client/windows_mobile/batty_agent/batty_agent/SettingForm.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace nayutaya.batty.agent
+{
+    partial class SettingForm
+    {
+        /// <summary>
+        /// 必要なデザイナ変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナで生成されたコード
+
+        /// <summary>
+        /// デザイナ サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディタで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.Menu = this.mainMenu1;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Text = "SettingForm";
+            this.AutoScroll = true;
+        }
+
+        #endregion
+    }
+}
---
+++ client/windows_mobile/batty_agent/batty_agent/SettingForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace nayutaya.batty.agent
+{
+    public partial class SettingForm : Form
+    {
+        public SettingForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
