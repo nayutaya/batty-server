3534714283754a9141d463bd606b2348c9df9841
Yuya Kato <yuyakato@gmail.com>
2009-06-14 22:22:07 +0900

    設定クラスにフィールドを追加

--- client/windows_mobile/batty_agent/batty_agent/Setting.cs
+++ client/windows_mobile/batty_agent/batty_agent/Setting.cs
@@ -7,5 +7,24 @@ namespace nayutaya.batty.agent
 {
     class Setting
     {
+        private String deviceToken;
+        private bool enableRecordOnBatteryCharging;
+        private bool enableRecordOnPowerConnecting;
+        private bool recordOnInterval;
+        private uint recordOnIntervalMinute;
+        private bool recordOnChangeLevelState;
+        private bool recordOnChangeChargeState;
+        private bool sendOnInterval;
+        private uint sendOnIntervalMinute;
+        private bool sendOnCount;
+        private uint sendOnCountRecords;
+        private bool sendOnChangeBatteryState;
+        private bool sendOnChangeChargeState;
+
+        public String DeviceToken
+        {
+            get { return deviceToken; }
+            set { deviceToken = value; }
+        }
     }
 }
