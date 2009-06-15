c55ba5a606ca2eb5e709b96f2b4462022a23afe3
Yuya Kato <yuyakato@gmail.com>
2009-06-15 16:26:14 +0900

    リファクタリング

--- client/windows_mobile/batty_agent/batty_agent/Setting.cs
+++ client/windows_mobile/batty_agent/batty_agent/Setting.cs
@@ -33,58 +33,10 @@ namespace nayutaya.batty.agent
             set { enableRecordOnBatteryCharging = value; }
         }
 
-        public bool SendOnChangeChargeState
+        public bool EnableRecordOnPowerConnecting
         {
-            get { return sendOnChangeChargeState; }
-            set { sendOnChangeChargeState = value; }
-        }
-
-        public bool SendOnChangeBatteryState
-        {
-            get { return sendOnChangeBatteryState; }
-            set { sendOnChangeBatteryState = value; }
-        }
-
-        public uint SendOnCountRecord
-        {
-            get { return sendOnCountRecord; }
-            set { sendOnCountRecord = value; }
-        }
-
-        public bool SendOnCount
-        {
-            get { return sendOnCount; }
-            set { sendOnCount = value; }
-        }
-
-        public uint SendOnIntervalMinute
-        {
-            get { return sendOnIntervalMinute; }
-            set { sendOnIntervalMinute = value; }
-        }
-
-        public bool SendOnInterval
-        {
-            get { return sendOnInterval; }
-            set { sendOnInterval = value; }
-        }
-
-        public bool RecordOnChangeChargeState
-        {
-            get { return recordOnChangeChargeState; }
-            set { recordOnChangeChargeState = value; }
-        }
-
-        public bool RecordOnChangeLevelState
-        {
-            get { return recordOnChangeLevelState; }
-            set { recordOnChangeLevelState = value; }
-        }
-
-        public uint RecordOnIntervalMinute
-        {
-            get { return recordOnIntervalMinute; }
-            set { recordOnIntervalMinute = value; }
+            get { return enableRecordOnPowerConnecting; }
+            set { enableRecordOnPowerConnecting = value; }
         }
 
         public bool RecordOnInterval
@@ -93,10 +45,58 @@ namespace nayutaya.batty.agent
             set { recordOnInterval = value; }
         }
 
-        public bool EnableRecordOnPowerConnecting
+        public uint RecordOnIntervalMinute
         {
-            get { return enableRecordOnPowerConnecting; }
-            set { enableRecordOnPowerConnecting = value; }
+            get { return recordOnIntervalMinute; }
+            set { recordOnIntervalMinute = value; }
+        }
+
+        public bool RecordOnChangeLevelState
+        {
+            get { return recordOnChangeLevelState; }
+            set { recordOnChangeLevelState = value; }
+        }
+
+        public bool RecordOnChangeChargeState
+        {
+            get { return recordOnChangeChargeState; }
+            set { recordOnChangeChargeState = value; }
+        }
+
+        public bool SendOnInterval
+        {
+            get { return sendOnInterval; }
+            set { sendOnInterval = value; }
+        }
+
+        public uint SendOnIntervalMinute
+        {
+            get { return sendOnIntervalMinute; }
+            set { sendOnIntervalMinute = value; }
+        }
+
+        public bool SendOnCount
+        {
+            get { return sendOnCount; }
+            set { sendOnCount = value; }
+        }
+
+        public uint SendOnCountRecord
+        {
+            get { return sendOnCountRecord; }
+            set { sendOnCountRecord = value; }
+        }
+
+        public bool SendOnChangeBatteryState
+        {
+            get { return sendOnChangeBatteryState; }
+            set { sendOnChangeBatteryState = value; }
+        }
+
+        public bool SendOnChangeChargeState
+        {
+            get { return sendOnChangeChargeState; }
+            set { sendOnChangeChargeState = value; }
         }
     }
 }
