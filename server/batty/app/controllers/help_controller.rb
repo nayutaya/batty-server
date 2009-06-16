21a4be69e8e5637d139177a8d75425a3659a00f3
Yuya Kato <yuyakato@gmail.com>
2009-06-16 16:32:05 +0900

    ヘルプコントローラにログインユーザの取得処理を追加

--- server/batty/app/controllers/help_controller.rb
+++ server/batty/app/controllers/help_controller.rb
@@ -1,6 +1,8 @@
 
 # ヘルプ
 class HelpController < ApplicationController
+  before_filter :authentication
+
   # GET /help
   def index
     # nop
--- server/batty/test/functional/help_controller_test.rb
+++ server/batty/test/functional/help_controller_test.rb
@@ -2,6 +2,10 @@
 require 'test_helper'
 
 class HelpControllerTest < ActionController::TestCase
+  def setup
+    @yuya = users(:yuya)
+  end
+
   test "routes" do
     base = {:controller => "help"}
 
@@ -9,11 +13,24 @@ class HelpControllerTest < ActionController::TestCase
     assert_routing("/help/any", base.merge(:action => "any"))
   end
 
-  test "GET index" do
+  test "GET index, logged in" do
+    session_login(@yuya)
+
     get :index
 
     assert_response(:success)
     assert_template("index")
+    assert_logged_in(@yuya)
+  end
+
+  test "GET index, no login" do
+    session_logout
+
+    get :index
+
+    assert_response(:success)
+    assert_template("index")
+    assert_not_logged_in
   end
 
   test "GET keywords" do
