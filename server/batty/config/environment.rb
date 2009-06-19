fee6d62dabee0f555e3308c6766bdd43183c301b
Yuya Kato <yuyakato@gmail.com>
2009-06-19 16:42:54 +0900

    ロケールをjaに固定

--- server/batty/config/environment.rb
+++ server/batty/config/environment.rb
@@ -48,6 +48,7 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  config.i18n.default_locale = :ja
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
