1041510172fdcd69999000b308f07f84afcc6bfa
Yuya Kato <yuyakato@gmail.com>
2009-07-09 18:23:00 +0900

    サインアップ画面のテストを追加

--- test/functional/signup_controller_test.rb
+++ test/functional/signup_controller_test.rb
@@ -7,4 +7,11 @@ class SignupControllerTest < ActionController::TestCase
 
     assert_routing("/signup", base.merge(:action => "index"))
   end
+
+  test "GET index" do
+    get :index
+
+    assert_response(:success)
+    assert_template("index")
+  end
 end
